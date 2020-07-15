bc4254d55528f54720140bbf2ec51151211de184
Denis Migranov <d.migranov@g.nsu.ru>
2020-07-15 18:52:02 +0700

    еще одна штука на будущее

---
+++ LBEngine/InputHandlerComponent.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Component.h"
+class InputHandlerComponent :
+    public Component
+{
+    //todo: чем-то похож на апдейтер и InputHandler: сделать также
+    //через лямбды.
+    //также сделать InputSystem...
+};
+
