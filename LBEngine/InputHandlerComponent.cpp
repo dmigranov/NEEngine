70210d92cc606b5f680d91169b78fa6f8ca8a4d8
Denis Migranov <d.migranov@g.nsu.ru>
2020-07-16 18:57:17 +0700

    начата работа над InputHandlerComponent

--- LBEngine/InputHandlerComponent.cpp
+++ LBEngine/InputHandlerComponent.cpp
@@ -1,2 +1,7 @@
 #include "pch.h"
 #include "InputHandlerComponent.h"
+
+InputHandlerComponent::InputHandlerComponent(std::function<void(Entity*, InputInfo)> func)
+{
+	m_inputHandlerFunc = func;
+}
--- LBEngine/InputHandlerComponent.h
+++ LBEngine/InputHandlerComponent.h
@@ -1,11 +1,22 @@
 #pragma once
 #include "Component.h"
+
+#include <functional>
+
 class InputHandlerComponent :
     public Component
 {
+public:
+    class InputInfo
+    {
+
+    };
+
     //todo: чем-то похож на апдейтер и InputHandler: сделать также
     //через лямбды.
-    //также сделать InputSystem...
-    InputHandlerComponent();
+    //также сделать InputSystem, апдейтер которой вызывает лямбду...
+    InputHandlerComponent(std::function<void(Entity *, InputInfo)> func);
+private:
+    std::function<void(Entity*, InputInfo)> m_inputHandlerFunc;
 };
 
