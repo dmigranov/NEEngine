85502aa064b5b898f5ef2b72f2d1430c2bdb0979
Denis Migranov <d.migranov@g.nsu.ru>
2020-07-17 16:37:47 +0700

    работа над таймером

--- LBEngine/inc/Timer.h
+++ LBEngine/inc/Timer.h
@@ -1,5 +1,15 @@
 #pragma once
+
+
 class Timer
 {
+public:
+	Timer();
+	~Timer();
+private:
+	DWORD m_timerStart;
+	DWORD m_stopwatchStart;
+	void Reset();
+
 };
 
--- LBEngine/src/Timer.cpp
+++ LBEngine/src/Timer.cpp
@@ -1,2 +1,13 @@
 #include "pch.h"
 #include "Timer.h"
+
+#include <ctime>
+
+Timer::Timer()
+{
+	m_timerStart = timeGetTime();
+	Reset();
+}
+
+Timer::~Timer()
+{}
