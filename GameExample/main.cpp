cd2188b9f7b1f684c88cd4f0ee22800a77be759d
Denis Migranov <d.migranov@g.nsu.ru>
2020-07-21 21:10:38 +0700

    todo для инпута

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -44,6 +44,16 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _
         scene->AddSystem(new BitmapRenderSystem());
     }
 
+    //todo: изменить InputSystem
+    //InputInfo -> InputComponent. Задумка: 
+    //I would create an input componentand various other action components, like walk, jump, crawl, weapon, etc.The input component would have fields that describe how the entity wants to move, such as : walk left, jump, and attack.This can be supplied by the keyboard, by an AI, or over the network, which is implemented as a control component.
+    //The control systems - there is one for each control component type(keyboard, gamepad, network, AI, etc.); they set fields of the input component
+    //The action systems - there is one for each action component type(walk, jump, attack, interact, etc.); they look at the fields of the input componentand modify the physics state, weapon component, etc.
+    //Тогда у InputHandler не будет зависимости от ускорения, сил...
+    //Если надо для Jump - ок
+    //Можно даже эти action systems реализовать в виде лямбд
+    //наследоваться от ActionSystem, выбирать нужные компоненты и лямбдить экшн
+
     Entity* cameraEntity = new Entity();
     auto cameraTransform = new TransformComponent(1, 0, -1, 0, 0, 0);
     cameraEntity->SetTransform(cameraTransform);
