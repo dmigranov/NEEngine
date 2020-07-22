6977f9e10ade0cb43dcd7511116d0a2dff96628c
Denis Migranov <d.migranov@g.nsu.ru>
2020-07-22 17:18:40 +0700

    physics system работа

--- LBEngine/PhysicsSystem.cpp
+++ LBEngine/PhysicsSystem.cpp
@@ -1,5 +1,8 @@
 #include "pch.h"
 #include "PhysicsSystem.h"
+#include "TransformComponent.h"
+#include "VelocityComponent.h"
+#include "Entity.h"
 
 PhysicsSystem::PhysicsSystem()
 {
@@ -10,4 +13,10 @@ PhysicsSystem::PhysicsSystem()
 
 void PhysicsSystem::Execute(DWORD deltaTime)
 {
+	for (auto pEntity : m_entities)
+	{
+		TransformComponent* transform = (TransformComponent*)pEntity->GetComponent(ComponentType::TransformComponentType);
+		VelocityComponent* velocity = (VelocityComponent*)pEntity->GetComponent(ComponentType::VelocityComponentType);
+
+	}
 }
