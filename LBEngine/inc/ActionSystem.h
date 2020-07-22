2591c92f8055b466a1ad48f3bede11bb1be36e3c
Denis Migranov <d.migranov@g.nsu.ru>
2020-07-22 15:46:45 +0700

    ActionSystem constructor signature

--- LBEngine/inc/ActionSystem.h
+++ LBEngine/inc/ActionSystem.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "System.h"
+
+#include <functional>
+
+
 class ActionSystem :
     public System
 {
+    ActionSystem(std::function<void(Entity*, DWORD)> func, ComponentType type, ...);
 };
 
--- LBEngine/src/ActionSystem.cpp
+++ LBEngine/src/ActionSystem.cpp
@@ -1,2 +1,7 @@
 #include "pch.h"
 #include "ActionSystem.h"
+#include "..\inc\ActionSystem.h"
+
+ActionSystem::ActionSystem(std::function<void(Entity*, DWORD)> func, ComponentType type, ...)
+{
+}
