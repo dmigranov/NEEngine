0acfd5434e4ce8d05c70e39f4262c6ad9b4a8aa3
Denis Migranov <d.migranov@g.nsu.ru>
2020-08-06 19:45:05 +0700

    pre collision system branching

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -52,7 +52,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _
     scene->AddSystem(new InputSystem());
     scene->AddSystem(new BitmapRenderSystem());
     scene->AddSystem(new PhysicsSystem());
-    //scene->AddSystem(new CollisionSystem());  //todo: оптимизировать
+    scene->AddSystem(new CollisionSystem());  //todo: оптимизировать
 
     auto cameraTransform = new TransformComponent(0, 0, -1, 0, 0, 0);
 
