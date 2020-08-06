fa72c9ecc8417ab125e58067d7b405cfb29cfdb1
Denis Migranov <d.migranov@g.nsu.ru>
2020-08-06 19:56:02 +0700

    добавлены конструкторы с isMovable

--- LBEngine/inc/CollisionComponent.h
+++ LBEngine/inc/CollisionComponent.h
@@ -6,8 +6,11 @@ class CollisionComponent :
 public:
     //todo: сделать конструктор с шириной и высотой и хранить их?
     CollisionComponent(std::function<void(Entity*, Entity*, double)> func, double upleftX, double upleftY, double downrightX, double downrightY);
+    CollisionComponent(std::function<void(Entity*, Entity*, double)> func, double upleftX, double upleftY, double downrightX, double downrightY, bool isMovable);
     CollisionComponent(std::function<void(Entity*, Entity*, double)> func, DirectX::SimpleMath::Vector2 upleft, DirectX::SimpleMath::Vector2 downright);
+    CollisionComponent(std::function<void(Entity*, Entity*, double)> func, DirectX::SimpleMath::Vector2 upleft, DirectX::SimpleMath::Vector2 downright, bool isMovable);
 
+    bool IsMovable();
 private:
     friend class CollisionSystem;
 
@@ -16,6 +19,6 @@ private:
     //AABB - axis-aligned bounding box
     DirectX::SimpleMath::Vector2 m_upleft, m_downright; //m_upright, m_downleft, - избыточно
 
-    bool isMovable = false;
+    bool m_isMovable = false;
 };
 
