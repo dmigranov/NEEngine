9ed608a3a893f7112f1d3a18d2a22e7f9452ef10
Denis Migranov <d.migranov@g.nsu.ru>
2020-08-11 19:30:30 +0700

    добавлена мапа и сигнатуры методов для импульсов в PhysicsComponent

--- LBEngine/inc/PhysicsComponent.h
+++ LBEngine/inc/PhysicsComponent.h
@@ -4,18 +4,21 @@
 #include <map>
 
 class Force;
-
+class Impulse;
 
 class PhysicsComponent :
     public Component
 {
 public:
-    
-
     PhysicsComponent(double mass);
+
     void AddForce(std::string forceName, Force force);
     Force GetForce(std::string forceName);
     void RemoveForce(std::string forceName);
+
+    void AddImpulse(std::string forceName, Impulse force);
+    Impulse GetImpulse(std::string forceName);
+    void RemoveImpulse(std::string forceName);
 
     void SetAcceleration(DirectX::SimpleMath::Vector3 a);
     void SetVelocity(DirectX::SimpleMath::Vector3 v);
@@ -24,7 +27,6 @@ public:
     const DirectX::SimpleMath::Vector3& GetAcceleration();
     const double& GetMass();
 
-
 private:
     friend class PhysicsSystem;
     DirectX::SimpleMath::Vector3 m_velocity;
@@ -32,5 +34,7 @@ private:
     double m_mass;
 
     std::map<std::string, Force> m_forces;
+    std::map<std::string, Impulse> m_impulses;
+
 };
 
