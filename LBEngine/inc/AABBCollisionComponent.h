37fee6df04a63f0a88b8685154ebcea376a2a0b3
Denis Migranov <d.migranov@g.nsu.ru>
2020-08-18 18:55:50 +0700

    начало работы по универсализации CollisionComponent

---
+++ LBEngine/inc/AABBCollisionComponent.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "CollisionComponent.h"
+
+//AABB - axis-aligned bounding box
+
+class AABBCollisionComponent :
+    public CollisionComponent
+{
+public:
+    AABBCollisionComponent(std::function<void(Entity*, Entity*, double)> func, double upleftX, double upleftY, double downrightX, double downrightY);
+    AABBCollisionComponent(std::function<void(Entity*, Entity*, double)> func, double upleftX, double upleftY, double downrightX, double downrightY, bool isMovable);
+    AABBCollisionComponent(std::function<void(Entity*, Entity*, double)> func, DirectX::SimpleMath::Vector2 upleft, DirectX::SimpleMath::Vector2 downright);
+    AABBCollisionComponent(std::function<void(Entity*, Entity*, double)> func, DirectX::SimpleMath::Vector2 upleft, DirectX::SimpleMath::Vector2 downright, bool isMovable);
+protected:
+
+private:
+    DirectX::SimpleMath::Vector2 m_upleft, m_downright; //m_upright, m_downleft, - избыточно
+
+};
+
--- LBEngine/inc/CollisionComponent.h
+++ LBEngine/inc/CollisionComponent.h
@@ -4,19 +4,11 @@ class CollisionComponent :
     public Component
 {
 public:
-    CollisionComponent(std::function<void(Entity*, Entity*, double)> func, double upleftX, double upleftY, double downrightX, double downrightY);
-    CollisionComponent(std::function<void(Entity*, Entity*, double)> func, double upleftX, double upleftY, double downrightX, double downrightY, bool isMovable);
-    CollisionComponent(std::function<void(Entity*, Entity*, double)> func, DirectX::SimpleMath::Vector2 upleft, DirectX::SimpleMath::Vector2 downright);
-    CollisionComponent(std::function<void(Entity*, Entity*, double)> func, DirectX::SimpleMath::Vector2 upleft, DirectX::SimpleMath::Vector2 downright, bool isMovable);
-
     bool IsMovable();
 private:
     friend class CollisionSystem;
 
     std::function<void(Entity*, Entity*, double)> m_executeFunc;
-
-    //AABB - axis-aligned bounding box
-    DirectX::SimpleMath::Vector2 m_upleft, m_downright; //m_upright, m_downleft, - избыточно
 
     bool m_isMovable = false;
 };
