070ad95461b9aab043c2c12d0f605b2c05bccd18
Denis Migranov <d.migranov@g.nsu.ru>
2020-08-20 15:09:45 +0700

    CircleCollisionComponent class done

--- LBEngine/inc/CircleCollisionComponent.h
+++ LBEngine/inc/CircleCollisionComponent.h
@@ -3,6 +3,14 @@
 class CircleCollisionComponent :
     public CollisionComponent
 {
+public:
+    CircleCollisionComponent(std::function<void(Entity*, Entity*, double)> func, DirectX::SimpleMath::Vector2 centre, double radius);
+    CircleCollisionComponent(std::function<void(Entity*, Entity*, double)> func, DirectX::SimpleMath::Vector2 centre, double radius, bool isMovable);
 
+    virtual CollisionComponentType GetType() override;
+
+private:
+    double m_radius;
+    DirectX::SimpleMath::Vector2 m_centre;
 };
 
--- LBEngine/src/CircleCollisionComponent.cpp
+++ LBEngine/src/CircleCollisionComponent.cpp
@@ -1,2 +1,21 @@
 #include "pch.h"
 #include "CircleCollisionComponent.h"
+
+#include "CollisionComponentType.h"
+
+CircleCollisionComponent::CircleCollisionComponent(std::function<void(Entity*, Entity*, double)> func, DirectX::SimpleMath::Vector2 centre, double radius)
+{
+	m_radius = radius;
+	m_centre = centre;
+	m_executeFunc = func;
+}
+
+CircleCollisionComponent::CircleCollisionComponent(std::function<void(Entity*, Entity*, double)> func, DirectX::SimpleMath::Vector2 centre, double radius, bool isMovable) : CircleCollisionComponent(func, centre, radius)
+{
+	m_isMovable = isMovable;
+}
+
+CollisionComponentType CircleCollisionComponent::GetType()
+{
+	return CollisionComponentType::CircleCollisionComponentType;
+}
