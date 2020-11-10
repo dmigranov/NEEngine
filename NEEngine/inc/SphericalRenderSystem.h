0718731ba57b6f6aa545bf0cd68bc31b5c17db85
Denis Migranov <d.migranov@g.nsu.ru>
2020-11-10 17:19:57 +0700

    создан файл для SphericalRenderSystem

---
+++ NEEngine/inc/SphericalRenderSystem.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "System.h"
+class SphericalRenderSystem :
+    public System
+{
+public:
+    SphericalRenderSystem();
+    virtual void Execute(double deltaTime) override;
+private:
+    void Render(Entity* pEntity, ID3D11DeviceContext* pDeviceContext, ID3D11Resource* pConstantBuffer);
+
+};
+
---
+++ NEEngine/src/SphericalRenderSystem.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "SphericalRenderSystem.h"
+
+SphericalRenderSystem::SphericalRenderSystem()
+{
+}
+
+void SphericalRenderSystem::Execute(double deltaTime)
+{
+}
+
+void SphericalRenderSystem::Render(Entity* pEntity, ID3D11DeviceContext* pDeviceContext, ID3D11Resource* pConstantBuffer)
+{
+}
