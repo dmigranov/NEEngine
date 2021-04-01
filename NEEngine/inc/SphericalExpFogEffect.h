ae87404b4e7f966da2b07aa3db4ed8cad9147bca
Denis Migranov <d.migranov@g.nsu.ru>
2021-04-01 13:58:18 +0700

    VertexPosTex в эффекте

--- NEEngine/inc/SphericalExpFogEffect.h
+++ NEEngine/inc/SphericalExpFogEffect.h
@@ -7,6 +7,12 @@ class SphericalExpFogEffect :
     public Effect
 {
 public:
+    struct VertexPosTex
+    {
+        DirectX::XMFLOAT4 Position;  //координаты точки в четырехмерном пространстве
+        DirectX::XMFLOAT2 TexCoord;
+    };
+
     // ”наследовано через Effect
     virtual bool Initialize() override;
     virtual bool Deinitialize() override;
@@ -16,8 +22,10 @@ protected:
     std::string magic = "SphericalExpFog";
 
 private:
-    std::string		m_vsFileName;
-    std::string		m_psFileName;
+    //std::string		m_vsFileName;
+    //std::string		m_psFileName;
+
+    ID3D11InputLayout* g_d3dInputLayout = nullptr;
 
     ID3D11VertexShader* g_d3dVertexShader = nullptr;
     ID3D11PixelShader* g_d3dPixelShader = nullptr;
