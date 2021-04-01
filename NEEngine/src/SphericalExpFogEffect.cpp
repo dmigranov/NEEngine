d1ac56a7c70c57aa5f1df5e4b6ea0a08d2ca4b58
Denis Migranov <d.migranov@g.nsu.ru>
2021-04-01 15:22:35 +0700

    сигнатура CreateInputLayout набросок

--- NEEngine/src/SphericalExpFogEffect.cpp
+++ NEEngine/src/SphericalExpFogEffect.cpp
@@ -22,7 +22,7 @@ bool SphericalExpFogEffect::Initialize()
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, offsetof(VertexData, Position), D3D11_INPUT_PER_VERTEX_DATA, 0 },
 		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 	};
-	g_d3dInputLayout = 
+	g_d3dInputLayout = game.CreateInputLayout(vertexLayoutDesc, _countof(vertexLayoutDesc), g_vs, sizeof(g_vs));
 
 	return true;
 }
