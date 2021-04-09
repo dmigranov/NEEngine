be3e645a6f3b8888534d71d073ceeb197fdcce24
Denis Migranov <d.migranov@g.nsu.ru>
2021-04-09 13:12:43 +0700

    идея для CreateSphericalMeshComponent

--- NEEngine/inc/MeshComponentFactory.h
+++ NEEngine/inc/MeshComponentFactory.h
@@ -8,6 +8,9 @@ public:
 	template<class VertexData> MeshComponent* CreateMeshComponent(int nv, VertexData* vertices, int ni, WORD* indices) {
 		//todo: создание и возвращение MeshComponent
 
+		//todo: создать класс CreateSphericalMeshComponent
+		//его методы: создание по списку вершин и индексов,
+		//но также и создание предопределнных объектов
 
 		return nullptr;
 	}
