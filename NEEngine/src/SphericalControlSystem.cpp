dbadfe8725bbf50d13e383074d97933cb9c094e0
Denis Migranov <d.migranov@g.nsu.ru>
2021-04-21 20:24:38 +0700

    SphericalControlSystem набросок

--- NEEngine/src/SphericalControlSystem.cpp
+++ NEEngine/src/SphericalControlSystem.cpp
@@ -1,2 +1,30 @@
 #include "pch.h"
 #include "SphericalControlSystem.h"
+
+#include "Entity.h"
+#include "InputComponent.h"
+#include "SphericalTransformComponent.h"
+
+SphericalControlSystem::SphericalControlSystem(double movementSpeed, double rotationSpeed) : ActionSystem([movementSpeed, rotationSpeed](Entity* pEntity, double deltaTime) {
+
+    auto pTransform = pEntity->GetComponent<SphericalTransformComponent>();
+    auto pInput = pEntity->GetComponent<InputComponent>();
+    auto kbs = pInput->GetKeyboardState();
+    auto ms = pInput->GetMouseState();
+
+    if (ms.leftButton)
+    {
+        double dx = ms.x * rotationSpeed * deltaTime, dy = ms.y * rotationSpeed * deltaTime;
+    }
+
+    if (kbs.W)
+        ;
+    if (kbs.S)
+        ;
+    if (kbs.A)
+        ;
+    if (kbs.D)
+        ;
+
+})
+{ }
