d1cc54b2eafe068482f4e7542a278477795b247b
Denis Migranov <d.migranov@g.nsu.ru>
2021-05-06 11:29:45 +0700

    начата реализация CreateSphericalSphere: инициализация необходимых структур

--- NEEngine/src/SphericalMeshComponentFactory.cpp
+++ NEEngine/src/SphericalMeshComponentFactory.cpp
@@ -1,9 +1,18 @@
 #include "pch.h"
 #include "SphericalMeshComponentFactory.h"
 
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
 MeshComponent* SphericalMeshComponentFactory::CreateSphericalSphere(double radius, int sliceCount, int stackCount, DirectX::XMFLOAT4 color)
 {
-	return nullptr;
+	int triCount = 0;
+
+	auto phiStep = XM_PI / stackCount;
+	auto thetaStep = XM_2PI / sliceCount;
+
+	std::vector<VertexPosColor> vertices;
+	std::vector<WORD> indices;
 }
 
 MeshComponent* SphericalMeshComponentFactory::CreateSphericalMeshFromFile()
