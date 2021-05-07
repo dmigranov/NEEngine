4df539d80f49cd3c8e5546bd1e46fc552ff2750b
Denis Migranov <d.migranov@g.nsu.ru>
2021-05-07 12:09:49 +0700

    приватные поля для SphericalControlSystem

--- NEEngine/inc/SphericalControlSystem.h
+++ NEEngine/inc/SphericalControlSystem.h
@@ -9,6 +9,21 @@ class SphericalCameraComponent;
 class SphericalControlSystem 
 	: public ActionSystem<InputComponent, SphericalTransformComponent, SphericalCameraComponent>
 {
+public:
 	SphericalControlSystem(double movementSpeed, double rotationSpeed);
+private:
+	DirectX::SimpleMath::Vector4 spherePos = DirectX::SimpleMath::Vector4(0, 0, 0, 1);
+
+
+	double pitchDelta = 0, yawDelta = 0;
+	double pitchLimit = DirectX::XM_PI / 2.0f - 0.01f;
+
+	DirectX::SimpleMath::Matrix T = DirectX::SimpleMath::Matrix::Identity; // represents the translation of the camera in world space
+	DirectX::SimpleMath::Matrix R = DirectX::SimpleMath::Matrix::Identity, 
+								RInv = DirectX::SimpleMath::Matrix::Identity, 
+								RYaw = DirectX::SimpleMath::Matrix::Identity, 
+								RPitch = DirectX::SimpleMath::Matrix::Identity, 
+								RRoll = DirectX::SimpleMath::Matrix::Identity; // represents camera orientation
+
 };
 
