df35d01db115bd6f1532c27b62da2809a0a88b86
Denis Migranov <d.migranov@g.nsu.ru>
2021-05-12 19:59:46 +0700

    подключение всех зависимостей и неймспейсов в EuclideanMeshComponentFactory

--- NEEngine/src/EuclideanMeshComponentFactory.cpp
+++ NEEngine/src/EuclideanMeshComponentFactory.cpp
@@ -1,2 +1,18 @@
 #include "pch.h"
 #include "EuclideanMeshComponentFactory.h"
+
+#include "MeshComponent.h"
+#include "Game.h"
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
+MeshComponent* EuclideanMeshComponentFactory::CreateSphere(double radius, int sliceCount, int stackCount)
+{
+    auto phiStep = XM_PI / stackCount;
+    auto thetaStep = XM_2PI / sliceCount;
+
+
+
+    return nullptr;
+}
