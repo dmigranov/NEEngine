bb5d5364e92241e7940d6070fda456a5733d193d
Denis Migranov <d.migranov@g.nsu.ru>
2021-05-12 22:44:00 +0700

    dependenices for toric effect

--- NEEngine/src/ToricExpFogEffect.cpp
+++ NEEngine/src/ToricExpFogEffect.cpp
@@ -1,6 +1,20 @@
 #include "pch.h"
 #include "ToricExpFogEffect.h"
 
+#include "Texture.h"
+#include "Scene.h"
+
+#include "SphericalMeshComponentFactory.h"	//для импорта структуры
+
+#include "SphericalTransformComponent.h"
+#include "SphericalCameraComponent.h"
+#include "Entity.h"
+
+//todo: include shaders 
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
 ToricExpFogEffect::ToricExpFogEffect(Texture* pTexture, double fogDensity, DirectX::XMVECTORF32 fogColor)
 {
 	m_magic = "ToricExpFog";
