594cc91dfaa721a40a87e638c78bd6c1a41a0767
Denis Migranov <d.migranov@g.nsu.ru>
2021-05-12 20:29:36 +0700

    получение необходимых для ренедринга вещей в ToricRenderSystem::Execute

--- NEEngine/src/ToricRenderSystem.cpp
+++ NEEngine/src/ToricRenderSystem.cpp
@@ -19,6 +19,9 @@ ToricRenderSystem::ToricRenderSystem(unsigned int replicationCount, double torX,
 
 void ToricRenderSystem::Execute(double deltaTime)
 {
+	auto& game = Game::GetInstance();
+	ID3D11DeviceContext* pDeviceContext = game.GetDeviceContext();
+	auto pDevice = game.GetDevice();
 }
 
 void ToricRenderSystem::Render(Entity* pEntity, ID3D11DeviceContext* pDeviceContext)
