e8a49206f3b42a24dd8cd00cb5f7fff508192b30
Denis Migranov <d.migranov@g.nsu.ru>
2021-05-12 22:58:53 +0700

    начало инициализации торического эффекта

--- NEEngine/src/ToricExpFogEffect.cpp
+++ NEEngine/src/ToricExpFogEffect.cpp
@@ -13,6 +13,7 @@
 #include "Game.h"
 
 //todo: include shaders 
+#include "PixelShader.h" 
 
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
@@ -28,8 +29,18 @@ ToricExpFogEffect::ToricExpFogEffect(Texture* pTexture, double fogDensity, Direc
 
 bool ToricExpFogEffect::Initialize()
 {
+	//shaders
+	//todo: vertex
+	g_d3dPixelShader = game.CreatePixelShaderFromBytecode(g_ps, sizeof(g_ps));
 
-	//g_d3dPixelShader = game.CreatePixelShaderFromBytecode(g_ps, sizeof(g_ps));
+	//input assembly:
+	D3D11_INPUT_ELEMENT_DESC vertexLayoutDesc[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, offsetof(EuclideanMeshComponentFactory::VertexData, Position), D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+	//g_d3dInputLayout = game.CreateInputLayout(vertexLayoutDesc, _countof(vertexLayoutDesc), g_torexpvs, sizeof(g_sphexpvs));
+
 
 	return false;
 }
