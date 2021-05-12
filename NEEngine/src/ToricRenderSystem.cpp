50bf8b0ec67c9c901bd54eb918b43367bdd45438
Denis Migranov <d.migranov@g.nsu.ru>
2021-05-12 20:40:50 +0700

    void ToricRenderSystem::Execute(double deltaTime) implemented

--- NEEngine/src/ToricRenderSystem.cpp
+++ NEEngine/src/ToricRenderSystem.cpp
@@ -1,8 +1,10 @@
 #include "pch.h"
 #include "ToricRenderSystem.h"
 
+#include "Entity.h"
 #include "TransformComponent.h"
 #include "MeshComponent.h"
+
 
 ToricRenderSystem::ToricRenderSystem(unsigned int replicationCount, double torX, double torY, double torZ)
 {
@@ -22,6 +24,19 @@ void ToricRenderSystem::Execute(double deltaTime)
 	auto& game = Game::GetInstance();
 	ID3D11DeviceContext* pDeviceContext = game.GetDeviceContext();
 	auto pDevice = game.GetDevice();
+
+	//Rasterizer Stage
+	game.SetupRasterizer();
+
+	//Output Merger Stage (merges the output from the pixel shader onto the color and depth buffers)
+	game.SetupOutputMerger();
+
+	//todo: более оптимальный перебор по эффектам (и вынести может куда-то сам перебор в олтдельынй класс?)
+	for (auto pEntity : m_entities)
+	{
+		if (pEntity->IsVisible())
+			Render(pEntity, pDeviceContext);
+	}
 }
 
 void ToricRenderSystem::Render(Entity* pEntity, ID3D11DeviceContext* pDeviceContext)
