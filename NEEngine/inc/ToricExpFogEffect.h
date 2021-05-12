a51472151ffdbfe1ba8bd35c43c420c314b438bd
Denis Migranov <d.migranov@g.nsu.ru>
2021-05-12 22:46:05 +0700

    private fields for toric effect

--- NEEngine/inc/ToricExpFogEffect.h
+++ NEEngine/inc/ToricExpFogEffect.h
@@ -27,7 +27,45 @@ public:
         DirectX::XMMATRIX world;
     };
 
+    struct PerApplicationPSConstantBuffer
+    {
+        DirectX::XMVECTORF32 fogColor = DirectX::Colors::CadetBlue;
+        float m_edgeThickness = 0.00f;
+    };
+
     ToricExpFogEffect(Texture* pTexture, double fogDensity, DirectX::XMVECTORF32 fogColor);
 
+
+private:
+    PerApplicationPSConstantBuffer perApplicationPSConstantBuffer;
+    PerApplicationVSConstantBuffer perApplicationVSConstantBuffer;
+
+    ID3D11InputLayout* g_d3dInputLayout = nullptr;
+
+    //ID3D11VertexShader* g_d3dVertexShaderCurrent = nullptr;
+    ID3D11VertexShader* g_d3dVertexShaderSph = nullptr;
+    ID3D11VertexShader* g_d3dVertexShaderEll = nullptr;
+
+    ID3D11PixelShader* g_d3dPixelShader = nullptr;
+
+    ID3D11SamplerState* g_d3dSamplerState = nullptr;
+
+    Texture* m_pTexture = nullptr;
+
+    //ID3D11Buffer* g_d3dVSConstantBuffer; //тк у каждого объекта свой эффект то только один
+    enum ConstantBuffer
+    {
+        CB_Application, //The application level constant buffer stores variables that rarely change. 
+        CB_Frame,       //The frame level constant buffer stores variables that change each frame. An example of a frame level shader variable would be the cameraТs view matrix which changes whenever the camera moves
+        CB_Object,      //The object level constant buffer stores variables that are different for every object being rendered. An example of an object level shader variable is the objectТs world matrix.
+        NumConstantBuffers
+    };
+    //three constant buffers: buffers are used to store shader variables that remain constant during current draw call. An example of a constant shader variable is the cameraТs projection matrix. Since the projection matrix will be the same for every vertex of the object, this variable does not need to be passed to the shader using vertex data.
+
+    ID3D11Buffer* g_d3dVSConstantBuffers[NumConstantBuffers];
+
+    ID3D11Buffer* g_d3dPSConstantBuffer = nullptr;
+
+    Game& game;// = Game::GetInstance();
 };
 
