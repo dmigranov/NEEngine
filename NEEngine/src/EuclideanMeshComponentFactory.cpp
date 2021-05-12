8d3f637114dc4ece6ad5035b6c3b5912406f4111
Denis Migranov <d.migranov@g.nsu.ru>
2021-05-12 20:05:03 +0700

    выделение структур для вершин плюс северный полюс в CreateSphere

--- NEEngine/src/EuclideanMeshComponentFactory.cpp
+++ NEEngine/src/EuclideanMeshComponentFactory.cpp
@@ -12,7 +12,12 @@ MeshComponent* EuclideanMeshComponentFactory::CreateSphere(double radius, int sl
     auto phiStep = XM_PI / stackCount;
     auto thetaStep = XM_2PI / sliceCount;
 
+    std::vector<VertexData> vertices;
+    std::vector<WORD> indices;
 
+    vertices.push_back({ XMFLOAT4(0.f, radius, 0.f, 1.f),
+        //XMFLOAT4(0.f, 1.f, 0.f, 0.f),     //normal
+        XMFLOAT2(0.f, 0.f) });	//North pole
 
     return nullptr;
 }
