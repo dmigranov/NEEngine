214b29eee5f6cfd343b3accb15ea73886c0c8576
Denis Migranov <d.migranov@g.nsu.ru>
2021-05-13 15:40:09 +0700

    vertexLayoutDesc исправлен для инстансинга

--- NEEngine/src/ToricExpFogEffect.cpp
+++ NEEngine/src/ToricExpFogEffect.cpp
@@ -38,7 +38,8 @@ bool ToricExpFogEffect::Initialize()
 	{
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, offsetof(EuclideanMeshComponentFactory::VertexData, Position), D3D11_INPUT_PER_VERTEX_DATA, 0 },
 		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-	};	//TODO: INSTANCE
+		{ "TEXCOORD", 1, DXGI_FORMAT_R32G32B32_FLOAT, 1, 0, D3D11_INPUT_PER_INSTANCE_DATA, 1 }, //для инстансинга
+	};	//четвертое значение - inputslot = 1, тк Each vertex buffer must be assigned to a different slot, а у нас два vertex buffer'а
 	g_d3dInputLayout = game.CreateInputLayout(vertexLayoutDesc, _countof(vertexLayoutDesc), g_torexpvs, sizeof(g_torexpvs));
 
 	//буферы:
