1e04ec67412f4112e0f6924d0555155c2d36cd32
Denis Migranov <d.migranov@g.nsu.ru>
2021-05-13 13:18:42 +0700

    ToricRenderSystem::Render - отправка буфферов вершиных и индексных

--- NEEngine/src/ToricRenderSystem.cpp
+++ NEEngine/src/ToricRenderSystem.cpp
@@ -4,6 +4,7 @@
 #include "Entity.h"
 #include "TransformComponent.h"
 #include "MeshComponent.h"
+#include "Effect.h"
 
 
 ToricRenderSystem::ToricRenderSystem(unsigned int replicationCount, double torX, double torY, double torZ)
@@ -55,6 +56,21 @@ void ToricRenderSystem::Render(Entity* pEntity, ID3D11DeviceContext* pDeviceCont
 		pEntity->SetVisible(false);
 		return;
 	}
+
+	// Input Assembler Stage - unique for every mesh
+	// Set the vertex buffer to active in the input assembler so it can be rendered.
+	unsigned int stride = pEffect->GetVertexBufferSize();
+	unsigned int offset = 0;
+
+	auto vertexBuffer = pMeshComponent->GetVertexBuffer();
+	auto indexBuffer = pMeshComponent->GetIndexBuffer();
+	pDeviceContext->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+	// Set the index buffer to active in the input assembler so it can be rendered.
+	pDeviceContext->IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R16_UINT, 0);
+	// Set the type of primitive that should be rendered from this vertex buffer, in this case triangles.
+	pDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+
 
 	//todo: в старой версии не учитывается смещение, чтобы объект был по центру!
 }
