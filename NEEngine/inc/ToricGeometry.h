6136bdcbd15eace04f01b471d7d9550688c4e70d
Denis Migranov <d.migranov@g.nsu.ru>
2021-07-14 18:46:17 +0700

    toric header done

--- NEEngine/inc/ToricGeometry.h
+++ NEEngine/inc/ToricGeometry.h
@@ -1,1 +1,26 @@
 #pragma once
+
+// Common
+
+#include <pch.h>
+
+#include "Game.h"
+#include "Scene.h"
+#include "ResourceManager.h"
+#include "InputComponent.h"
+#include "Entity.h"
+
+// Components
+
+#include "ComponentTypeManager.h"
+
+#include "MeshComponent.h"
+#include "TransformComponent.h"
+#include "EuclideanMeshComponentFactory.h"
+
+// Systems
+#include "ToricRenderSystem.h"
+#include "CameraActionSystem.h"
+
+// Effects
+#include "ToricExpFogEffect.h"
