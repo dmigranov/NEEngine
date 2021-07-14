7e9262da905b6648d342616b052337756fab5eb7
Denis Migranov <d.migranov@g.nsu.ru>
2021-07-14 18:41:58 +0700

    toric visualization in main

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -1,8 +1,39 @@
 ﻿#pragma once
 
-#include "SphericalEllipticGeometry.h"
+#include <pch.h>
+
+#include "Game.h"
+#include "Scene.h"
+#include "ResourceManager.h"
+#include "InputComponent.h"
+#include "Entity.h"
+
+// Components
+#include "ComponentTypeManager.h"
+
+#include "InputComponent.h"
+#include "BitmapComponent.h"
 #include "WalkComponent.h"
+#include "MeshComponent.h"
+#include "TransformComponent.h"
+
+
+#include "MeshComponentFactory.h"
+#include "EuclideanMeshComponentFactory.h"
+
+// Systems
+#include "TransformUpdateSystem.h"
 #include "InputSystem.h"
+#include "ActionSystem.h"
+
+#include "ToricRenderSystem.h"
+
+#include "CameraActionSystem.h"
+
+
+//Effect
+#include "ToricExpFogEffect.h"
+
 
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
@@ -21,8 +52,6 @@ int main(int argc, char* argv[])
         componentTypeManager->RegisterComponentType<InputComponent>();
         componentTypeManager->RegisterComponentType<WalkComponent>();
 
-        componentTypeManager->RegisterComponentType<SphericalTransformComponent>();
-        componentTypeManager->RegisterComponentType<SphericalCameraComponent>();
         componentTypeManager->RegisterComponentType<CameraComponent>();
 
         //transfcomp и meshcom зарегистр по умолчанию...
@@ -33,80 +62,39 @@ int main(int argc, char* argv[])
     Texture* brickTexture = resourceManager->CreateTexture(L"brick.dds");
     Texture* earthTexture = resourceManager->CreateTexture(L"earth8k.dds");
     Texture* characterTexture = resourceManager->CreateTexture(L"char2.dds");
+    Texture* cubemapTexture = resourceManager->CreateTexture(L"cubemap.dds");
 
     scene->AddSystem(new InputSystem());
-    scene->AddSystem(new SphericalRenderSystem());
-    scene->AddSystem(new SphericalControlSystem(0.3, 1.3));
-    scene->AddSystem(new ActionSystem<InputComponent, SphericalTransformComponent, WalkComponent>(
-        [](Entity* pEntity, double deltaTime) {
-        auto pTransform = pEntity->GetComponent<SphericalTransformComponent>();
-        auto pInput = pEntity->GetComponent<InputComponent>();
-        auto kbs = pInput->GetKeyboardState();
-        auto ms = pInput->GetMouseState();
-        auto pWalk = pEntity->GetComponent<WalkComponent>();
-
-
-        Vector3 up(0, deltaTime * pWalk->m_movementGain, 0);
-        Vector3 right(deltaTime * pWalk->m_movementGain, 0, 0);
-        Vector3 fwd(0, 0, deltaTime * pWalk->m_movementGain);
-
-        /*
-        if (kbs.D)
-            pTransform->SetWorld(SphericalRotationXW(deltaTime * pWalk->m_movementGain) * pTransform->GetWorld());
-        if (kbs.A)
-            pTransform->SetWorld(SphericalRotationXW(-deltaTime * pWalk->m_movementGain) * pTransform->GetWorld());
-        if (kbs.W)
-            pTransform->SetWorld(SphericalRotationYW(deltaTime * pWalk->m_movementGain) * pTransform->GetWorld());
-        if (kbs.S)
-            pTransform->SetWorld(SphericalRotationYW(-deltaTime * pWalk->m_movementGain) * pTransform->GetWorld());
-        */
-        if (kbs.R)
-            pTransform->SetWorld(SphericalRotationZW(deltaTime * pWalk->m_movementGain) * pTransform->GetWorld());
-        if (kbs.F)
-            pTransform->SetWorld(SphericalRotationZW(-deltaTime * pWalk->m_movementGain) * pTransform->GetWorld());
-
-        //pTransform->Move(-fwd); //должно быть так todo
-    }));
-
+    scene->AddSystem(new CameraActionSystem());
+    scene->AddSystem(new ToricRenderSystem(5, 30, 30, 30));
 
     Entity* cameraEntity = new Entity("camera1");
-    auto cameraTransform = new SphericalTransformComponent();
-    auto cameraComponent = new SphericalCameraComponent();
-    cameraEntity->AddComponent<SphericalTransformComponent>(cameraTransform);
-    cameraEntity->AddComponent<SphericalCameraComponent>(cameraComponent);
+    auto cameraTransform = new TransformComponent();
+    auto cameraComponent = new CameraComponent();
+    cameraEntity->AddComponent<TransformComponent>(cameraTransform);
+    cameraEntity->AddComponent<CameraComponent>(cameraComponent);
     cameraEntity->AddComponent<InputComponent>(new InputComponent());
     scene->SetCamera(cameraEntity, cameraComponent);
     scene->AddEntity(cameraEntity);
 
 
-    auto effect = new SphericalExpFogEffect(earthTexture, 0.1, DirectX::Colors::PowderBlue);
+    auto effect = new ToricExpFogEffect(cubemapTexture, 0.02, DirectX::Colors::PowderBlue);
 
 
     auto charWalkComponent = new WalkComponent(3, 4);
     auto charInputComponent = new InputComponent();
 
     auto testEntity = new Entity();
-    auto stc = new SphericalTransformComponent();
+    auto stc = new TransformComponent();
 
-    auto smc = SphericalMeshComponentFactory::CreateSphericalSphere(0.3, 20, 20);
+    auto smc = EuclideanMeshComponentFactory::CreateCube(3);
     smc->SetEffect(effect);
-    testEntity->AddComponent<SphericalTransformComponent>(stc);
+    testEntity->AddComponent<TransformComponent>(stc);
     testEntity->AddComponent<MeshComponent>(smc);
     testEntity->AddComponent<WalkComponent>(charWalkComponent);
     testEntity->AddComponent<InputComponent>(charInputComponent);
     scene->AddEntity(testEntity);
 
-    scene->AddSystem(new ActionSystem<InputComponent>(
-        [effect](Entity* pEntity, double deltaTime) {
-        auto pInput = pEntity->GetComponent<InputComponent>();
-        auto kbs = pInput->GetKeyboardState();
-        auto sphEff = (SphericalExpFogEffect*)effect;
-
-        if (kbs.D1)
-            sphEff->SetMode(true);
-        else if (kbs.D2)
-            sphEff->SetMode(false);
-    }));
 
     return game.StartGame();
 }
