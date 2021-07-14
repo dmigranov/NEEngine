862a90e1d59b5db023c3647e29ee92b304313499
Denis Migranov <d.migranov@g.nsu.ru>
2021-07-14 18:33:09 +0700

    SphericalEllipticGeometry.h filling

--- NEEngine/inc/SphericalEllipticGeometry.h
+++ NEEngine/inc/SphericalEllipticGeometry.h
@@ -1,1 +1,11 @@
 #pragma once
+
+// Math
+#include "SphericalMath.h" //todo: remove - should be in TransformComponent instead
+
+// Systems
+#include "SphericalRenderSystem.h"
+#include "SphericalControlSystem.h"
+
+// Effects
+#include "SphericalExpFogEffect.h"
