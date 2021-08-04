185ff0e3b0ad807bf6bd987073e3118fa53c8721
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-04 12:29:00 +0700

    some common stuff added to hyp geo header

--- NEEngine/inc/Geometries/HyperbolicGeometry.h
+++ NEEngine/inc/Geometries/HyperbolicGeometry.h
@@ -1,1 +1,18 @@
 #pragma once
+
+// Common
+
+#include <pch.h>
+
+#include "Game.h"
+#include "Scene.h"
+#include "ResourceManager.h"
+#include "InputComponent.h"
+#include "Entity.h"
+
+// Components
+
+#include "ComponentTypeManager.h"
+
+#include "MeshComponent.h"
+//todo: hyperbolic stuff 
