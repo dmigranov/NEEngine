984ac86075b7ccb111da1b73263df44d38d24973
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-04 13:50:16 +0700

    HyperbolicRenderSystem::Execute implemented

--- NEEngine/src/HyperbolicRenderSystem.cpp
+++ NEEngine/src/HyperbolicRenderSystem.cpp
@@ -13,3 +13,28 @@ HyperbolicRenderSystem::HyperbolicRenderSystem()
 
 	m_isDrawing = true;
 }
+
+void HyperbolicRenderSystem::Execute(double deltaTime)
+{
+	auto& game = Game::GetInstance();
+	ID3D11DeviceContext* pDeviceContext = game.GetDeviceContext();
+	auto pDevice = game.GetDevice();
+
+	//Rasterizer Stage
+	game.SetupRasterizer();
+
+	//Output Merger Stage (merges the output from the pixel shader onto the color and depth buffers)
+	game.SetupOutputMerger();
+
+	//todo: более оптимальный перебор по эффектам (и вынести может куда-то сам перебор в олтдельынй класс?)
+	for (auto pEntity : m_entities)
+	{
+		if (pEntity->IsVisible())
+			Render(pEntity, pDeviceContext);
+	}
+}
+
+void HyperbolicRenderSystem::Render(Entity* pEntity, ID3D11DeviceContext* pDeviceContext)
+{
+	
+}
