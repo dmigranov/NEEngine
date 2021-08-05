69851c3d47838fb57519790f575f332bbf1baa65
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-05 11:19:54 +0700

    private fields: HyperbolicExpFogEffect; fixing errors

--- NEEngine/src/HyperbolicExpFogEffect.cpp
+++ NEEngine/src/HyperbolicExpFogEffect.cpp
@@ -9,13 +9,18 @@
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
 
-HyperbolicExpFogEffect::HyperbolicExpFogEffect(Texture* pTexture, double fogDensity, DirectX::XMVECTORF32 fogColor)
+HyperbolicExpFogEffect::HyperbolicExpFogEffect(Texture* pTexture, double fogDensity, DirectX::XMVECTORF32 fogColor) : game(Game::GetInstance())
 {
 	m_magic = "HyperbolicExpFog";
 	m_pTexture = pTexture;
 	perApplicationVSConstantBuffer.density = fogDensity;
 	perApplicationPSConstantBuffer.fogColor = fogColor;
 	Initialize();
+}
+
+bool HyperbolicExpFogEffect::Initialize()
+{
+	return false;
 }
 
 void HyperbolicExpFogEffect::SetFogColor(DirectX::XMVECTORF32 fogColor)
