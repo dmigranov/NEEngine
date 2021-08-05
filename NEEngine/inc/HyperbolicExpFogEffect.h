6e562d870b3840f59dda1fbfd7ff60cd9c7b406c
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-05 11:11:30 +0700

    public methods declarations for HyperbolicExpFogEffect

--- NEEngine/inc/HyperbolicExpFogEffect.h
+++ NEEngine/inc/HyperbolicExpFogEffect.h
@@ -8,6 +8,26 @@ class HyperbolicExpFogEffect :
     public Effect
 {
 public:
+
+    //todo: structs
+
     HyperbolicExpFogEffect(Texture* pTexture, double fogDensity, DirectX::XMVECTORF32 fogColor);
+    
+    virtual bool Initialize() override;
+    virtual void Deinitialize() override;
+
+    virtual void UpdatePerObject(const Entity* pEntity) override;
+    virtual void UpdatePerScene() override;             //todo: impl
+    virtual void UpdatePerApplication() override;       //todo: impl
+
+    virtual unsigned int GetVertexBufferSize() const override;
+    virtual const std::type_info& GetVertexDataType() override;
+
+    void SetFogColor(DirectX::XMVECTORF32 fogColor);
+
+    ~HyperbolicExpFogEffect();
+
+private:
+
 };
 
