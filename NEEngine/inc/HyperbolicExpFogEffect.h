3c4bf1fb45914a9c25ad9a215ffed89634a5749f
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-05 11:05:08 +0700

    HyperbolicExpFogEffect constructor declaration

--- NEEngine/inc/HyperbolicExpFogEffect.h
+++ NEEngine/inc/HyperbolicExpFogEffect.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "Effect.h"
+
+class Game;
+class Texture;
+
 class HyperbolicExpFogEffect :
     public Effect
 {
+public:
+    HyperbolicExpFogEffect(Texture* pTexture, double fogDensity, DirectX::XMVECTORF32 fogColor);
 };
 
--- NEEngine/src/HyperbolicExpFogEffect.cpp
+++ NEEngine/src/HyperbolicExpFogEffect.cpp
@@ -1,2 +1,9 @@
 #include "pch.h"
 #include "HyperbolicExpFogEffect.h"
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
+HyperbolicExpFogEffect::HyperbolicExpFogEffect(Texture* pTexture, double fogDensity, DirectX::XMVECTORF32 fogColor)
+{
+}
