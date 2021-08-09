a71b043e7b15cf20bc08beed2926b2a044cdfdd2
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-09 16:58:40 +0700

    input and output structs for shader

--- NEEngine/Data/Shaders/VertexHyperbolicExpFogShader.hlsl
+++ NEEngine/Data/Shaders/VertexHyperbolicExpFogShader.hlsl
@@ -14,6 +14,20 @@ cbuffer PerObject : register(b2)
 	matrix worldMatrix;
 }
 
+//POSITION and COLOR are semantics that are used to link vs variables to ps variables
+struct VertexShaderInput
+{
+	float4 position : POSITION;
+	float2 tex : TEXCOORD0;
+};
+
+struct VertexShaderOutput
+{
+	float2 tex : TEXCOORD0;
+	float fogFactor : FOG_FACTOR;
+	float4 position : SV_POSITION; //должно быть последним при поступлении в пиксельный шейдер, если в нем не будем его брать (иначе всё сместится)
+};
+
 float4 main( float4 pos : POSITION ) : SV_POSITION
 {
 	return pos;
