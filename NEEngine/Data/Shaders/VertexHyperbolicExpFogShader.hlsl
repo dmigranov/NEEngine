6ab3f4567fd82e3cb8f5bfa7411297d6e6f01441
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-09 16:58:09 +0700

    vertex buffers

--- NEEngine/Data/Shaders/VertexHyperbolicExpFogShader.hlsl
+++ NEEngine/Data/Shaders/VertexHyperbolicExpFogShader.hlsl
@@ -1,3 +1,19 @@
+cbuffer PerApplication : register(b0)
+{
+	matrix projectionMatrix;
+	double density;
+}
+
+cbuffer PerFrame : register(b1)
+{
+	matrix viewMatrix;
+}
+
+cbuffer PerObject : register(b2)
+{
+	matrix worldMatrix;
+}
+
 float4 main( float4 pos : POSITION ) : SV_POSITION
 {
 	return pos;
