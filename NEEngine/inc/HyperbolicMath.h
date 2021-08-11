955333951155cde42f0d53107cca15e016caa9c8
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-11 16:14:33 +0700

    double -> float hyperbolic + HyperbolicTranslationXW

--- NEEngine/inc/HyperbolicMath.h
+++ NEEngine/inc/HyperbolicMath.h
@@ -1,8 +1,9 @@
 #pragma once
 
-DirectX::SimpleMath::Matrix HyperbolicTranslationZW(double d);
-DirectX::SimpleMath::Matrix HyperbolicTranslationXW(double d);
-DirectX::SimpleMath::Matrix HyperbolicTranslationYW(double d);
-DirectX::SimpleMath::Matrix HyperbolicRotationYZ(double d);
-DirectX::SimpleMath::Matrix HyperbolicRotationXZ(double d);
-DirectX::SimpleMath::Matrix HyperbolicRotationXY(double d);
+DirectX::SimpleMath::Matrix HyperbolicTranslationZW(float d);
+DirectX::SimpleMath::Matrix HyperbolicTranslationXW(float d);
+DirectX::SimpleMath::Matrix HyperbolicTranslationYW(float d);
+
+DirectX::SimpleMath::Matrix HyperbolicRotationYZ(float d);
+DirectX::SimpleMath::Matrix HyperbolicRotationXZ(float d);
+DirectX::SimpleMath::Matrix HyperbolicRotationXY(float d);
