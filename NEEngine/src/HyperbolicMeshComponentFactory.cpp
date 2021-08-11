1af5bc1af2ec402f902722480350d0d4fc04ee43
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-11 14:17:02 +0700

    all vertices for hyp sphere defined

--- NEEngine/src/HyperbolicMeshComponentFactory.cpp
+++ NEEngine/src/HyperbolicMeshComponentFactory.cpp
@@ -24,5 +24,27 @@ MeshComponent* HyperbolicMeshComponentFactory::CreateHyperbolicSphere(double rad
 		//XMFLOAT4(0.f, 1.f, 0.f, 0.f),     //normal
 		XMFLOAT2(0.f, 0.f) });	//North pole
 
+    for (int i = 1; i <= stackCount - 1; i++) {
+        auto phi = i * phiStep;
+        for (int j = 0; j <= sliceCount; j++) {
+            auto theta = j * thetaStep;
+            XMFLOAT4 pos(
+                (radius * sinf(phi) * cosf(theta)),
+                (radius * cosf(phi)),
+                (radius * sinf(phi) * sinf(theta)),
+                height
+            );
+
+            auto uv = XMFLOAT2(theta / XM_2PI, phi / XM_PI);
+            vertices.push_back({ pos,
+                //-pos,       //так как это гиперб. (сумма -1)! 
+                uv });
+        }
+    }
+
+    vertices.push_back({ XMFLOAT4(0.f, -radius, 0.f, height),
+        //XMFLOAT4(0.f, -1.f, 0.f, 0.f),    //normal
+        XMFLOAT2(0.f, 1.f) }); //south pole
+
     return nullptr;
 }
