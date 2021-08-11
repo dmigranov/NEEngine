5864b1525a8de3b512b7d259bf716e2b7ccf0997
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-11 16:15:04 +0700

    tabulations for hyper math

--- NEEngine/src/HyperbolicMath.cpp
+++ NEEngine/src/HyperbolicMath.cpp
@@ -7,26 +7,26 @@ using namespace DirectX;
 
 DirectX::SimpleMath::Matrix HyperbolicTranslationXW(float d)
 {
-	return Matrix(coshf(d), 0.f, 0.f, coshf(d),
-		0.f, 1.f, 0.f, 0.f,
-		0.f, 0.f, 1.f, 0.f,
-		sinhf(d), 0.f, 0.f, coshf(d));
+	return Matrix(	coshf(d), 0.f, 0.f, coshf(d),
+					0.f, 1.f, 0.f, 0.f,
+					0.f, 0.f, 1.f, 0.f,
+					sinhf(d), 0.f, 0.f, coshf(d));
 }
 
 DirectX::SimpleMath::Matrix HyperbolicRotationYZ(float d)
 {
 	return Matrix(	1.f, 0.f, 0.f, 0.f,
-				0.f, cosf(d), -sinf(d), 0.f,
-				0.f, sinf(d), cosf(d), 0.f,
-				0.f, 0.f, 0.f, 1.f);
+					0.f, cosf(d), -sinf(d), 0.f,
+					0.f, sinf(d), cosf(d), 0.f,
+					0.f, 0.f, 0.f, 1.f);
 }
 
 DirectX::SimpleMath::Matrix HyperbolicRotationXZ(float d)
 {
 	return Matrix(	cosf(d), 0.f, -sinf(d), 0.f,
-				0.f, 1.f, 0.f, 0.f,
-				sinf(d), 0.f, cosf(d), 0.f,
-				0.f, 0.f, 0.f, 1.f);
+					0.f, 1.f, 0.f, 0.f,
+					sinf(d), 0.f, cosf(d), 0.f,
+					0.f, 0.f, 0.f, 1.f);
 }
 
 DirectX::SimpleMath::Matrix HyperbolicRotationXY(float d)
