498c64383e7144d0fc00c4519897fca0668caeca
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-11 14:03:51 +0700

    MeshComponent* HyperbolicMeshComponentFactory::CreateHyperbolicSphere: defiining necessary data structures and variables

--- NEEngine/src/HyperbolicMeshComponentFactory.cpp
+++ NEEngine/src/HyperbolicMeshComponentFactory.cpp
@@ -9,5 +9,13 @@ using namespace DirectX::SimpleMath;
 
 MeshComponent* HyperbolicMeshComponentFactory::CreateHyperbolicSphere(double radius, int sliceCount, int stackCount)
 {
+	int triCount = 0;
+
+	auto phiStep = XM_PI / stackCount;
+	auto thetaStep = XM_2PI / sliceCount;
+
+	std::vector<VertexData> vertices;
+	std::vector<WORD> indices;
+
     return nullptr;
 }
