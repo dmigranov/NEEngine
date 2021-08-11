58f5190a19c9dadc7d63f646aac68d8d6762ad79
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-11 14:03:19 +0700

    includes and namespaces

--- NEEngine/src/HyperbolicMeshComponentFactory.cpp
+++ NEEngine/src/HyperbolicMeshComponentFactory.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
 #include "HyperbolicMeshComponentFactory.h"
 
+#include "MeshComponent.h"
+#include "Game.h"
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
 MeshComponent* HyperbolicMeshComponentFactory::CreateHyperbolicSphere(double radius, int sliceCount, int stackCount)
 {
     return nullptr;
