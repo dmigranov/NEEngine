302ac82822351aecfcef1efdd348275864287043
Denis Migranov <d.migranov@g.nsu.ru>
2021-08-11 16:11:03 +0700

    hyperb rotations

--- NEEngine/src/HyperbolicMath.cpp
+++ NEEngine/src/HyperbolicMath.cpp
@@ -3,3 +3,27 @@
 
 using namespace DirectX::SimpleMath;
 using namespace DirectX;
+
+DirectX::SimpleMath::Matrix HyperbolicRotationYZ(double d)
+{
+	return Matrix(1.f, 0.f, 0.f, 0.f,
+		0.f, cosf(d), -sinf(d), 0.f,
+		0.f, sinf(d), cosf(d), 0.f,
+		0.f, 0.f, 0.f, 1.f);
+}
+
+DirectX::SimpleMath::Matrix HyperbolicRotationXZ(double d)
+{
+	return Matrix(cosf(d), 0.f, -sinf(d), 0.f,
+		0.f, 1.f, 0.f, 0.f,
+		sinf(d), 0.f, cosf(d), 0.f,
+		0.f, 0.f, 0.f, 1.f);
+}
+
+DirectX::SimpleMath::Matrix HyperbolicRotationXY(double d)
+{
+	return Matrix(cosf(d), -sinf(d), 0.f, 0.f,
+		sinf(d), cosf(d), 0.f, 0.f,
+		0.f, 0.f, 1.f, 0.f,
+		0.f, 0.f, 0.f, 1.f);
+}
