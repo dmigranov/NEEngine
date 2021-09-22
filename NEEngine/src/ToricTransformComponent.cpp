52a6016de1e7bbcf39367fdafce4b80d310a6c17
Denis Migranov <d.migranov@g.nsu.ru>
2021-09-22 10:38:36 +0700

    ToricTransformComponent conbstructors implemented

--- NEEngine/src/ToricTransformComponent.cpp
+++ NEEngine/src/ToricTransformComponent.cpp
@@ -1,2 +1,17 @@
 #include "pch.h"
 #include "ToricTransformComponent.h"
+
+ToricTransformComponent::ToricTransformComponent() : TransformComponent()
+{ }
+
+ToricTransformComponent::ToricTransformComponent(double x, double y, double z) : TransformComponent(x, y, z)
+{ }
+
+ToricTransformComponent::ToricTransformComponent(DirectX::SimpleMath::Vector3 position) : TransformComponent(position)
+{ }
+
+ToricTransformComponent::ToricTransformComponent(double x, double y, double z, double rx, double ry, double rz, double sx, double sy, double sz) : TransformComponent(x, y, z, rx, ry, rz, sx, sy, sz)
+{ }
+
+ToricTransformComponent::ToricTransformComponent(DirectX::SimpleMath::Vector3 position, DirectX::SimpleMath::Vector3 rotation, DirectX::SimpleMath::Vector3 scale) : TransformComponent(position, rotation, scale)
+{ }
