e8f617cdab7c3c714ff4004efff60a91d80420da
Denis Migranov <d.migranov@g.nsu.ru>
2021-10-06 14:38:08 +0700

    included common.h into spherical geom include file

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -1,9 +1,8 @@
 ﻿#pragma once
 
-#include "Geometries/HyperbolicGeometry.h"
+#include "Geometries/SphericalEllipticGeometry.h"
 #include "WalkComponent.h"
 #include "InputSystem.h"
-#include "ActionSystem.h"
 
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
@@ -22,8 +21,8 @@ int main(int argc, char* argv[])
         componentTypeManager->RegisterComponentType<InputComponent>();
         componentTypeManager->RegisterComponentType<WalkComponent>();
 
-        componentTypeManager->RegisterComponentType<HyperbolicTransformComponent>();
-        componentTypeManager->RegisterComponentType<HyperbolicCameraComponent>();
+        componentTypeManager->RegisterComponentType<SphericalTransformComponent>();
+        componentTypeManager->RegisterComponentType<SphericalCameraComponent>();
 
         //transfcomp и meshcom зарегистр по умолчанию... 
 
@@ -33,11 +32,11 @@ int main(int argc, char* argv[])
     Texture* earthTexture = resourceManager->CreateTexture(L"earth8k.dds");
 
     scene->AddSystem(new InputSystem());
-    scene->AddSystem(new HyperbolicRenderSystem());
-    scene->AddSystem(new HyperbolicControlSystem(0.3, 1.3));
-    scene->AddSystem(new ActionSystem<InputComponent, HyperbolicTransformComponent, WalkComponent>(
+    scene->AddSystem(new SphericalRenderSystem());
+    scene->AddSystem(new SphericalControlSystem(0.3, 1.3));
+    scene->AddSystem(new ActionSystem<InputComponent, SphericalTransformComponent, WalkComponent>(
         [](Entity* pEntity, double deltaTime) {
-            auto pTransform = pEntity->GetComponent<HyperbolicTransformComponent>();
+            auto pTransform = pEntity->GetComponent<SphericalTransformComponent>();
             auto pInput = pEntity->GetComponent<InputComponent>();
             auto kbs = pInput->GetKeyboardState();
             auto ms = pInput->GetMouseState();
@@ -48,57 +47,65 @@ int main(int argc, char* argv[])
             Vector3 right(deltaTime * pWalk->m_movementGain, 0, 0);
             Vector3 fwd(0, 0, deltaTime * pWalk->m_movementGain);
 
+
             if (kbs.R)
                 pTransform->Move(up);
             if (kbs.F)
                 pTransform->Move(-up);
 
+            if (kbs.T)
+                pTransform->Rotate(deltaTime, 0, 0);
+            if (kbs.G)
+                pTransform->Rotate(0, deltaTime, 0);
+            if (kbs.B)
+                pTransform->Rotate(0, 0, deltaTime);
         }));
 
 
     Entity* cameraEntity = new Entity("camera1");
-    auto cameraTransform = new HyperbolicTransformComponent();
-    auto cameraComponent = new HyperbolicCameraComponent();
-    cameraEntity->AddComponent<HyperbolicTransformComponent>(cameraTransform);
-    cameraEntity->AddComponent<HyperbolicCameraComponent>(cameraComponent);
+    auto cameraTransform = new SphericalTransformComponent();
+    auto cameraComponent = new SphericalCameraComponent();
+    cameraEntity->AddComponent<SphericalTransformComponent>(cameraTransform);
+    cameraEntity->AddComponent<SphericalCameraComponent>(cameraComponent);
     cameraEntity->AddComponent<InputComponent>(new InputComponent());
-    //cameraEntity->AddComponent<WalkComponent>(new WalkComponent(3, 4));
-
     scene->SetCamera(cameraEntity, cameraComponent);
     scene->AddEntity(cameraEntity);
 
 
-    auto effect = new HyperbolicExpFogEffect(earthTexture, 0.5, DirectX::Colors::PowderBlue);
+    auto effect = new SphericalExpFogEffect(earthTexture, 0.1, DirectX::Colors::PowderBlue);
 
 
     auto charWalkComponent = new WalkComponent(3, 4);
     auto charInputComponent = new InputComponent();
 
-    auto entity1 = new Entity(), entity2 = new Entity(), entity3 = new Entity(), entity4 = new Entity();
+    auto entity1 = new Entity(), entity2 = new Entity();
 
-    auto smc = HyperbolicMeshComponentFactory::CreateHyperbolicSphere(0.3, 20, 20);
+    auto smc = SphericalMeshComponentFactory::CreateSphericalSphere(0.3, 20, 20);
     smc->SetEffect(effect);
 
-    auto tc1 = new HyperbolicTransformComponent(0, 0, 0);
-    auto tc2 = new HyperbolicTransformComponent(0.7, 0, 0);
-    auto tc3 = new HyperbolicTransformComponent(1.4, 0, 0);
-    auto tc4 = new HyperbolicTransformComponent(2.1, 0, 0);
+    auto tc1 = new SphericalTransformComponent(-1, 0, 0);
+    auto tc2 = new SphericalTransformComponent(1, 0, 0);
 
-    entity1->AddComponent<HyperbolicTransformComponent>(tc1);
+    entity1->AddComponent<SphericalTransformComponent>(tc1);
     entity1->AddComponent<MeshComponent>(smc);
+    entity1->AddComponent<WalkComponent>(charWalkComponent);
+    entity1->AddComponent<InputComponent>(charInputComponent);
     scene->AddEntity(entity1);
 
-    entity2->AddComponent<HyperbolicTransformComponent>(tc2);
+    entity2->AddComponent<SphericalTransformComponent>(tc2);
     entity2->AddComponent<MeshComponent>(smc);
     scene->AddEntity(entity2);
 
-    entity3->AddComponent<HyperbolicTransformComponent>(tc3);
-    entity3->AddComponent<MeshComponent>(smc);
-    scene->AddEntity(entity3);
+    scene->AddSystem(new ActionSystem<InputComponent>(
+        [effect](Entity* pEntity, double deltaTime) {
+            auto pInput = pEntity->GetComponent<InputComponent>();
+            auto kbs = pInput->GetKeyboardState();
 
-    entity4->AddComponent<HyperbolicTransformComponent>(tc4);
-    entity4->AddComponent<MeshComponent>(smc);
-    scene->AddEntity(entity4);
+            if (kbs.D1)
+                effect->SetMode(true);
+            else if (kbs.D2)
+                effect->SetMode(false);
+        }));
 
     return game.StartGame();
-}
+} 
--- NEEngine/inc/Geometries/SphericalEllipticGeometry.h
+++ NEEngine/inc/Geometries/SphericalEllipticGeometry.h
@@ -1,24 +1,11 @@
 #pragma once
 
 // Common
+#include "Common.h"
 
-#include <pch.h>
-
-#include "Game.h"
-#include "Scene.h"
-#include "ResourceManager.h"
-#include "InputComponent.h"
-#include "Entity.h"
-
-// Math
-
-#include "SphericalMath.h" //todo: remove - should be in TransformComponent instead
 
 // Components
 
-#include "ComponentTypeManager.h"
-
-#include "MeshComponent.h"
 #include "SphericalTransformComponent.h"
 #include "SphericalCameraComponent.h"
 #include "SphericalMeshComponentFactory.h"
