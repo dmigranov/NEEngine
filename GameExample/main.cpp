e96ed4b1529af377147bfdf84715ca80932b7d84
Denis Migranov <d.migranov@g.nsu.ru>
2021-10-06 14:35:08 +0700

    hyperb geometry: common.h added

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -1,90 +1,104 @@
 ﻿#pragma once
 
-#include "Geometries/ToricGeometry.h"
+#include "Geometries/HyperbolicGeometry.h"
+#include "WalkComponent.h"
+#include "InputSystem.h"
+#include "ActionSystem.h"
 
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
 
 int main(int argc, char* argv[])
 {
-    auto scene = InitializeToricGeometry(L"Test game", DirectX::Colors::PowderBlue,
-        8, DirectX::SimpleMath::Vector3(30, 30, 30), 10, 0.9,
-        true, false, false);
+    Game& game = Game::GetInstance();
+    game.InitializeEngine(L"Test game", true, false, false);
+    game.SetBackgroundColor(DirectX::Colors::PowderBlue);   //todo: перенести
+    Scene* scene = game.GetScene();
+    auto resourceManager = game.GetResourceManager();
 
-    auto resourceManager = Game::GetInstance().GetResourceManager();
-    Texture* cubemapTexture = resourceManager->CreateTexture(L"cubemap.dds");
-
-
-    Entity* cameraEntity = new Entity("camera1");
-    auto cameraTransform = new ToricTransformComponent();
-    auto cameraComponent = new CameraComponent();
-    cameraEntity->AddComponent<ToricTransformComponent>(cameraTransform);
-    cameraEntity->AddComponent<CameraComponent>(cameraComponent);
-    cameraEntity->AddComponent<InputComponent>(new InputComponent());
-    scene->SetCamera(cameraEntity, cameraComponent);
-    scene->AddEntity(cameraEntity);
-
-
-    auto effect = new ToricExpFogEffect(cubemapTexture, 0.015, DirectX::Colors::PowderBlue);
-
-    //auto charWalkComponent = new WalkComponent(200, 4);
-    auto charInputComponent = new InputComponent();
-
-    auto entity1 = new Entity();
-    auto ttc1 = new ToricTransformComponent();
     {
-        auto tmc1 = EuclideanMeshComponentFactory::CreateCube(3);
-        tmc1->SetEffect(effect);
-        entity1->AddComponent<ToricTransformComponent>(ttc1);
-        entity1->AddComponent<MeshComponent>(tmc1);
-        entity1->AddComponent<InputComponent>(charInputComponent);
-        scene->AddEntity(entity1);
+        auto componentTypeManager = game.GetComponentTypeManager();
+        componentTypeManager->RegisterComponentType<CameraComponent>();
+        componentTypeManager->RegisterComponentType<InputComponent>();
+        componentTypeManager->RegisterComponentType<WalkComponent>();
 
-        entity1->AddComponent<UpdaterComponent>(new UpdaterComponent([](double deltaTime, Entity* pEntity) {
-            auto pTransform = pEntity->GetComponent<ToricTransformComponent>();
+        componentTypeManager->RegisterComponentType<HyperbolicTransformComponent>();
+        componentTypeManager->RegisterComponentType<HyperbolicCameraComponent>();
+
+        //transfcomp и meshcom зарегистр по умолчанию... 
+
+        componentTypeManager->SetTypeAdditionEnded();
+    }
+
+    Texture* earthTexture = resourceManager->CreateTexture(L"earth8k.dds");
+
+    scene->AddSystem(new InputSystem());
+    scene->AddSystem(new HyperbolicRenderSystem());
+    scene->AddSystem(new HyperbolicControlSystem(0.3, 1.3));
+    scene->AddSystem(new ActionSystem<InputComponent, HyperbolicTransformComponent, WalkComponent>(
+        [](Entity* pEntity, double deltaTime) {
+            auto pTransform = pEntity->GetComponent<HyperbolicTransformComponent>();
             auto pInput = pEntity->GetComponent<InputComponent>();
             auto kbs = pInput->GetKeyboardState();
             auto ms = pInput->GetMouseState();
+            auto pWalk = pEntity->GetComponent<WalkComponent>();
 
-            Vector3 up(0, deltaTime * 200, 0);
-            Vector3 right(deltaTime * 200, 0, 0);
-            Vector3 fwd(0, 0, deltaTime * 200);
+
+            Vector3 up(0, deltaTime * pWalk->m_movementGain, 0);
+            Vector3 right(deltaTime * pWalk->m_movementGain, 0, 0);
+            Vector3 fwd(0, 0, deltaTime * pWalk->m_movementGain);
 
             if (kbs.R)
                 pTransform->Move(up);
             if (kbs.F)
                 pTransform->Move(-up);
 
-            if (kbs.T)
-                pTransform->Rotate(deltaTime, 0, 0);
-            if (kbs.Y)
-                pTransform->Rotate(-deltaTime, 0, 0);
-            if (kbs.G)
-                pTransform->Rotate(0, deltaTime, 0);
-            if (kbs.H)
-                pTransform->Rotate(0, -deltaTime, 0);
-            if (kbs.B)
-                pTransform->Rotate(0, 0, deltaTime);
-            if (kbs.N)
-                pTransform->Rotate(0, 0, -deltaTime);
+        }));
 
-            }));
-    }
 
-    {
-        auto childEntity = new Entity();
-        auto ttc2 = new ToricTransformComponent(3, 0, 0);
-        ttc2->SetParent(ttc1);
-        auto tmc2 = EuclideanMeshComponentFactory::CreateCube(1);
-        tmc2->SetEffect(effect);
-        childEntity->AddComponent<ToricTransformComponent>(ttc2);
-        childEntity->AddComponent<MeshComponent>(tmc2);
-        scene->AddEntity(childEntity);
+    Entity* cameraEntity = new Entity("camera1");
+    auto cameraTransform = new HyperbolicTransformComponent();
+    auto cameraComponent = new HyperbolicCameraComponent();
+    cameraEntity->AddComponent<HyperbolicTransformComponent>(cameraTransform);
+    cameraEntity->AddComponent<HyperbolicCameraComponent>(cameraComponent);
+    cameraEntity->AddComponent<InputComponent>(new InputComponent());
+    //cameraEntity->AddComponent<WalkComponent>(new WalkComponent(3, 4));
 
-        childEntity->AddComponent<UpdaterComponent>(new UpdaterComponent([](double delta, Entity* pEntity) {
-            pEntity->GetComponent<ToricTransformComponent>()->RotateGlobal(0, 2 * delta, 0);
-            }));
-    }
+    scene->SetCamera(cameraEntity, cameraComponent);
+    scene->AddEntity(cameraEntity);
 
-    return Game::GetInstance().StartGame();
+
+    auto effect = new HyperbolicExpFogEffect(earthTexture, 0.5, DirectX::Colors::PowderBlue);
+
+
+    auto charWalkComponent = new WalkComponent(3, 4);
+    auto charInputComponent = new InputComponent();
+
+    auto entity1 = new Entity(), entity2 = new Entity(), entity3 = new Entity(), entity4 = new Entity();
+
+    auto smc = HyperbolicMeshComponentFactory::CreateHyperbolicSphere(0.3, 20, 20);
+    smc->SetEffect(effect);
+
+    auto tc1 = new HyperbolicTransformComponent(0, 0, 0);
+    auto tc2 = new HyperbolicTransformComponent(0.7, 0, 0);
+    auto tc3 = new HyperbolicTransformComponent(1.4, 0, 0);
+    auto tc4 = new HyperbolicTransformComponent(2.1, 0, 0);
+
+    entity1->AddComponent<HyperbolicTransformComponent>(tc1);
+    entity1->AddComponent<MeshComponent>(smc);
+    scene->AddEntity(entity1);
+
+    entity2->AddComponent<HyperbolicTransformComponent>(tc2);
+    entity2->AddComponent<MeshComponent>(smc);
+    scene->AddEntity(entity2);
+
+    entity3->AddComponent<HyperbolicTransformComponent>(tc3);
+    entity3->AddComponent<MeshComponent>(smc);
+    scene->AddEntity(entity3);
+
+    entity4->AddComponent<HyperbolicTransformComponent>(tc4);
+    entity4->AddComponent<MeshComponent>(smc);
+    scene->AddEntity(entity4);
+
+    return game.StartGame();
 }
--- NEEngine/inc/Geometries/HyperbolicGeometry.h
+++ NEEngine/inc/Geometries/HyperbolicGeometry.h
@@ -2,23 +2,11 @@
 
 // Common
 
-#include <pch.h>
+#include "Common.h"
 
-#include "Game.h"
-#include "Scene.h"
-#include "ResourceManager.h"
-#include "InputComponent.h"
-#include "Entity.h"
-
-// Math
-
-#include "SphericalMath.h" //todo: remove - should be in TransformComponent instead
 
 // Components
 
-#include "ComponentTypeManager.h"
-
-#include "MeshComponent.h"
 #include "HyperbolicTransformComponent.h"
 #include "HyperbolicCameraComponent.h"
 #include "HyperbolicMeshComponentFactory.h"
