bf2c852da35cab94d92a16148c25242378a65784
Denis Migranov <d.migranov@g.nsu.ru>
2021-11-07 12:05:34 +0700

    some

--- GameExample/RandomSpherical.cpp
+++ GameExample/RandomSpherical.cpp
@@ -1,1 +1,11 @@
+#include "pch.h"
 #include "RandomSpherical.h"
+
+RandomSpherical::RandomSpherical(double radius)
+{
+}
+
+DirectX::SimpleMath::Vector4 RandomSpherical::GeneratePoint()
+{
+	return DirectX::SimpleMath::Vector4();
+}
--- GameExample/RandomSpherical.h
+++ GameExample/RandomSpherical.h
@@ -1,5 +1,8 @@
 #pragma once
 class RandomSpherical
 {
+public:
+	RandomSpherical(double radius);
+	DirectX::SimpleMath::Vector4 GeneratePoint();
 };
 
