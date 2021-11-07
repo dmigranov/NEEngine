4fc54b8c554082f795b2fd0ee0c9283bdcbd2852
Denis Migranov <d.migranov@g.nsu.ru>
2021-11-07 15:58:10 +0700

    TextComponent incldues align

--- NEEngine/inc/TextComponent.h
+++ NEEngine/inc/TextComponent.h
@@ -13,8 +13,8 @@ class TextComponent :
     public Component
 {
 public:
-    TextComponent(const char* m_text, const float x, const float y);
-    TextComponent(std::function<const char* (double delta)> func, const float x, const float y);
+    TextComponent(const char* m_text, const float x, const float y, Alignment align);
+    TextComponent(std::function<const char* (double delta)> func, const float x, const float y, Alignment align);
 
     const DirectX::SimpleMath::Vector2 GetPosition() const;
     const char* GetText(double delta = 0) const;
@@ -22,5 +22,6 @@ private:
     const char* m_text = nullptr;
     const float m_x, m_y;
     std::function<const char* (double delta)> m_func;
+    Alignment m_align;
 };
 
