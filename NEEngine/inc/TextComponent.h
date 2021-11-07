c3001ecb69e32626440362e7c2aab039ede5562f
Denis Migranov <d.migranov@g.nsu.ru>
2021-11-07 15:35:52 +0700

    TextComponent(std::function<char* (double delta)> func, const float x, const float y);

--- NEEngine/inc/TextComponent.h
+++ NEEngine/inc/TextComponent.h
@@ -3,8 +3,12 @@
 class TextComponent :
     public Component
 {
+public:
+    TextComponent(const char* m_text, const float x, const float y);
+    TextComponent(std::function<char* (double delta)> func, const float x, const float y);
 private:
-    const char* m_text;
+    const char* m_text = nullptr;
     const float m_x, m_y;
+    std::function<char* (double delta)> m_func;
 };
 
--- NEEngine/src/TextComponent.cpp
+++ NEEngine/src/TextComponent.cpp
@@ -1,2 +1,10 @@
 #include "pch.h"
 #include "TextComponent.h"
+
+TextComponent::TextComponent(const char* text, const float x, const float y) : m_x(x), m_y(y), m_text(text)
+{ }
+
+TextComponent::TextComponent(std::function<char* (double delta)> func, const float x, const float y) : m_x(x), m_y(y)
+{
+	m_func = func;
+}
