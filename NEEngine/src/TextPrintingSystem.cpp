db4e792adf72508eebf2e74ed128632d1e388477
Denis Migranov <d.migranov@g.nsu.ru>
2021-11-07 15:45:56 +0700

    some text print system woek

--- NEEngine/src/TextPrintingSystem.cpp
+++ NEEngine/src/TextPrintingSystem.cpp
@@ -1,6 +1,13 @@
 #include "pch.h"
 #include "TextPrintingSystem.h"
 
+#include "TextComponent.h"
 #include "TextDrawer.h"
 
+TextPrintingSystem::TextPrintingSystem()
+{
+	SubscribeToComponentType<TextComponent>();
 
+
+	m_isDrawing = true;
+}
