aeb0985ea4b5922f2be433ed76aa6be34f248743
Denis Migranov <d.migranov@g.nsu.ru>
2021-11-07 12:34:27 +0700

    generation of correct xyzw

--- GameExample/RandomSpherical.cpp
+++ GameExample/RandomSpherical.cpp
@@ -10,6 +10,7 @@ RandomSpherical::RandomSpherical(double radius) : m_gen(rd()), m_distr(-radius, 
 
 DirectX::SimpleMath::Vector4 RandomSpherical::GeneratePoint()
 {
+generate:
 	double x = m_distr(m_gen);
 	double y = m_distr(m_gen);
 	double z = m_distr(m_gen);
@@ -17,7 +18,10 @@ DirectX::SimpleMath::Vector4 RandomSpherical::GeneratePoint()
 
 	double norm_square = x * x + y * y + z * z + w * w;
 
-	//if ()
+	if (norm_square < epsilon || norm_square > m_radius_square) //too close too zero OR outside the sphere
+		goto generate;
+
+	//todo: normalize
 
 	return DirectX::SimpleMath::Vector4();
 }
--- GameExample/RandomSpherical.h
+++ GameExample/RandomSpherical.h
@@ -8,6 +8,7 @@ public:
 	DirectX::SimpleMath::Vector4 GeneratePoint();
 private: 
 	double m_radius, m_radius_square;
+	double epsilon = 0.001;
 	std::random_device rd;  // Will be used to obtain a seed for the random number engine
 	std::mt19937 m_gen;
 	std::uniform_real_distribution<> m_distr;
