4b437513ea6d3af5ad2b0a9de5d4d2baa8779323
Denis Migranov <d.migranov@g.nsu.ru>
2021-11-24 18:05:34 +0700

    эллипт шейдер не работает

--- NEEngine/Data/Shaders/VertexEllipticalExpFogShader.hlsl
+++ NEEngine/Data/Shaders/VertexEllipticalExpFogShader.hlsl
@@ -62,13 +62,20 @@ VertexShaderOutput main(VertexShaderInput IN, uint instanceID : SV_InstanceID)
 
 	matrix viewWorld = mul(viewMatrix, worldMatrix);
 
+	float4 position; //итоговая позиция
 	float4 position1 = normalize(IN.position); //нормализованные координаты: лежат на единичной гиперсфере
 	float4 objectCenter1 = float4(0, 0, 0, 1); //координаты центра объекта для единичной гиперсферы в координатах world
 	float distanceFromPointToCenter = SphericalDistance(position1, objectCenter1, 1); //must stay the same!
-	float w_new = radius * (1 - 2 * pow(sin(distanceFromPointToCenter / (2 * radius)), 2));
-	float lambda = sqrt((position1.x * position1.x + position1.y * position1.y + position1.z * position1.z) / (radius * radius - w_new * w_new));
-	float x_new = position1.x / lambda, y_new = position1.y / lambda, z_new = position1.z / lambda;
-	float4 position = float4(x_new, y_new, z_new, w_new);
+	if (distanceFromPointToCenter < epsilon)
+		position = IN.position;
+	else
+	{
+		float w_new = radius * (1 - 2 * pow(sin(distanceFromPointToCenter / (2 * radius)), 2));
+		float lambda = sqrt((position1.x * position1.x + position1.y * position1.y + position1.z * position1.z) / (radius * radius - w_new * w_new));
+		float x_new = position1.x / lambda, y_new = position1.y / lambda, z_new = position1.z / lambda;
+		position = float4(x_new, y_new, z_new, w_new);
+	}
+
 
 	float4 cameraSpacePosition = mul(viewWorld, position);
 	//float chordLength = distance(float4(0, 0, 0, radius), cameraSpacePosition); //длина хорды
