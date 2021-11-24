cf6f5dc3765f6ecb0429988e7dfe9fe97a1ed19a
Denis Migranov <d.migranov@g.nsu.ru>
2021-11-24 14:51:03 +0700

    public methods of SphericalExpFogPointEffect

--- NEEngine/inc/SphericalExpFogPointEffect.h
+++ NEEngine/inc/SphericalExpFogPointEffect.h
@@ -3,6 +3,7 @@
 class SphericalExpFogPointEffect :
     public SphericalExpFogEffect
 {
+public:
     SphericalExpFogPointEffect(Texture* pTexture, double fogDensity, DirectX::XMVECTORF32 fogColor);
 
     virtual bool Initialize() override;
