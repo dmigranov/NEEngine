7d03b997b9738ab8f94706d97d780f81a3eeed5b
Denis Migranov <d.migranov@g.nsu.ru>
2021-11-24 17:51:59 +0700

    понятно почему: не нормализовано

--- NEEngine/Data/Shaders/BasicPixelShader.hlsl
+++ NEEngine/Data/Shaders/BasicPixelShader.hlsl
@@ -14,7 +14,7 @@ struct PixelShaderInput
  
 float4 SimplePixelShader(PixelShaderInput IN) : SV_TARGET
 {
-	float4 sourceColor = ((IN.tex.x == 0.f && IN.tex.y == 0.f) ? 0.f : shaderTexture.Sample(SampleType, IN.tex));
+	float4 sourceColor = ((IN.tex.x == 0.f && IN.tex.y == 0.f) ? 1.f : shaderTexture.Sample(SampleType, IN.tex));
 	float4 retColor = IN.fogFactor * sourceColor + (1.0 - IN.fogFactor) * fogColor;
 
 	return retColor;
