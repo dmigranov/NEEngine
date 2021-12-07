d559b3261911d65097172b804e1f7eeb553a15af
Denis Migranov <d.migranov@g.nsu.ru>
2021-12-07 12:10:18 +0700

    bool SphericalDopplerEffect::Initialize()  done

--- NEEngine/src/SphericalDopplerEffect.cpp
+++ NEEngine/src/SphericalDopplerEffect.cpp
@@ -13,6 +13,23 @@
 
 SphericalDopplerEffect::SphericalDopplerEffect(Texture* pTexture, double fogDensity, DirectX::XMVECTORF32 fogColor) : SphericalExpFogEffect(pTexture, fogDensity, fogColor)
 {
-	m_magic = "SphericalExpFogPoint";
+	m_magic = "SphericalDoppler";
 	Initialize(); //parent Initialize already called
+}
+
+bool SphericalDopplerEffect::Initialize()
+{
+	SafeRelease(g_d3dPixelShader);
+	g_d3dPixelShader = game.CreatePixelShaderFromBytecode(g_psd, sizeof(g_psd));
+	D3D11_BUFFER_DESC constantBufferDesc;
+	ZeroMemory(&constantBufferDesc, sizeof(D3D11_BUFFER_DESC));
+	constantBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	constantBufferDesc.CPUAccessFlags = 0;
+	constantBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	constantBufferDesc.ByteWidth = sizeof(PerApplicationPSConstantBufferDoppler);
+
+	SafeRelease(g_d3dPSConstantBuffer);
+	g_d3dPSConstantBuffer = game.CreateBuffer(constantBufferDesc);
+
+	return true;
 }
