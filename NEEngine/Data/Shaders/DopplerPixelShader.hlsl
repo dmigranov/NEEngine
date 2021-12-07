077ca3c1fcd0b561cb8da9d66c69b8b4f6a8ffb9
Denis Migranov <d.migranov@g.nsu.ru>
2021-12-07 13:24:45 +0700

    started float3 rgb2hsv(float3 rgb)

--- NEEngine/Data/Shaders/DopplerPixelShader.hlsl
+++ NEEngine/Data/Shaders/DopplerPixelShader.hlsl
@@ -13,10 +13,15 @@ struct PixelShaderInput
 	float fogFactor : FOG_FACTOR;
 };
 
+float3 rgb2hsv(float3 rgb)
+{
+
+}
+
 float4 main(PixelShaderInput IN) : SV_TARGET
 {
 	float4 sourceColor = ((IN.tex.x == 0.f && IN.tex.y == 0.f) ? 1.f : shaderTexture.Sample(SampleType, IN.tex));
-	
+
 	float4 retColor = IN.fogFactor * sourceColor + (1.0 - IN.fogFactor) * fogColor;
 	return retColor;
 }
