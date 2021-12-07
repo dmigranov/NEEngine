3eeab9139382fce785d77dae60af1b3283a9a911
Denis Migranov <d.migranov@g.nsu.ru>
2021-12-07 12:03:56 +0700

    class SphericalDopplerEffect :     public SphericalExpFogEffect

--- NEEngine/inc/SphericalDopplerEffect.h
+++ NEEngine/inc/SphericalDopplerEffect.h
@@ -1,27 +1,10 @@
 #pragma once
-#include "SphericalEffect.h"
+#include "SphericalExpFogEffect.h"
 class SphericalDopplerEffect :
-    public SphericalEffect
+    public SphericalExpFogEffect
 {
-    struct PerApplicationVSConstantBuffer
-    {
-        DirectX::XMMATRIX projFront;
-        DirectX::XMMATRIX projBack;
-        double density = 0.1;
-        double radius = 1.;
-    };
-
-    struct PerFrameVSConstantBuffer
-    {
-        DirectX::XMMATRIX view;
-    };
-
-    struct PerObjectVSConstantBuffer
-    {
-        DirectX::XMMATRIX world;
-    };
-
-    struct PerApplicationPSConstantBuffer
+public:
+    struct PerApplicationPSConstantBufferDoppler
     {
         DirectX::XMVECTORF32 fogColor = DirectX::Colors::CadetBlue;
         double velocity;
@@ -29,7 +12,14 @@ class SphericalDopplerEffect :
 
     SphericalDopplerEffect(Texture* pTexture, double fogDensity, DirectX::XMVECTORF32 fogColor);
 
+    virtual bool Initialize() override;
+    virtual void Deinitialize() override;
 
+    virtual void UpdatePerObject(const Entity* pEntity) override;
+
+    virtual void Clean() override;
+
+private:
 
 };
 
