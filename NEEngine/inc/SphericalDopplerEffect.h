405f46bd141887b4be715b73eabbfd97dd707db8
Denis Migranov <d.migranov@g.nsu.ru>
2021-12-07 11:41:12 +0700

    started SphericalDopplerEffect

--- NEEngine/inc/SphericalDopplerEffect.h
+++ NEEngine/inc/SphericalDopplerEffect.h
@@ -3,5 +3,32 @@
 class SphericalDopplerEffect :
     public SphericalEffect
 {
+    struct PerApplicationVSConstantBuffer
+    {
+        DirectX::XMMATRIX projFront;
+        DirectX::XMMATRIX projBack;
+        double density = 0.1;
+        double radius = 1.;
+    };
+
+    struct PerFrameVSConstantBuffer
+    {
+        DirectX::XMMATRIX view;
+    };
+
+    struct PerObjectVSConstantBuffer
+    {
+        DirectX::XMMATRIX world;
+    };
+
+    struct PerApplicationPSConstantBuffer
+    {
+        DirectX::XMVECTORF32 fogColor = DirectX::Colors::CadetBlue;
+        double velocity;
+    };
+
+    SphericalDopplerEffect(Texture* pTexture, double fogDensity, DirectX::XMVECTORF32 fogColor);
+
+
 };
 
