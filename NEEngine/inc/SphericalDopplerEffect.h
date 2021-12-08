a074a644dabbf9bfe428a0322cf1ee9e15f8efcd
Denis Migranov <d.migranov@g.nsu.ru>
2021-12-08 16:57:46 +0700

    почему то только для одной сферы...

--- NEEngine/inc/SphericalDopplerEffect.h
+++ NEEngine/inc/SphericalDopplerEffect.h
@@ -7,7 +7,7 @@ public:
     struct PerApplicationPSConstantBufferDoppler 
     {
         DirectX::XMVECTORF32 fogColor = DirectX::Colors::CadetBlue;
-        double velocity = 0.;
+        double velocity = 80000000.;
     };
 
     struct PerApplicationVSConstantBufferDoppler
