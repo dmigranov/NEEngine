0eb57dc4800fea408e8775186178b7f9ed5cf8b2
Denis Migranov <d.migranov@g.nsu.ru>
2021-12-21 12:53:07 +0700

    bad

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -9,6 +9,56 @@
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
 
+LRESULT CALLBACK WndProcFriedmann(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    static bool s_in_sizemove = false;
+    static bool s_in_suspend = false;
+    static bool s_minimized = false;
+    static bool s_fullscreen = false; // TODO: Set s_fullscreen to true if defaulting to fullscreen
+
+    switch (message)
+    {
+    case WM_PAINT:          
+        hdc = BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+        break;
+    case WM_ACTIVATEAPP:
+        Keyboard::ProcessMessage(message, wParam, lParam);
+        Mouse::ProcessMessage(message, wParam, lParam);
+        break;
+ 
+    case WM_INPUT:
+    case WM_MOUSEMOVE:
+    case WM_LBUTTONDOWN:
+    case WM_LBUTTONUP:
+    case WM_RBUTTONDOWN:
+    case WM_RBUTTONUP:
+    case WM_MBUTTONDOWN:
+    case WM_MBUTTONUP:
+    case WM_MOUSEWHEEL:
+    case WM_XBUTTONDOWN:
+    case WM_XBUTTONUP:
+    case WM_MOUSEHOVER:
+        Mouse::ProcessMessage(message, wParam, lParam);
+        break;
+    case WM_KEYDOWN:
+    case WM_KEYUP:
+    case WM_SYSKEYUP:
+        Keyboard::ProcessMessage(message, wParam, lParam);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
 float SphericalDistance(Vector4 vec1, Vector4 vec2, double radius)
 {
     float chordLength = Vector4::Distance(vec1, vec2); //chord length
@@ -47,20 +97,33 @@ int main(int argc, char* argv[])
         WNDCLASSEXW wcex = {};
         wcex.cbSize = sizeof(WNDCLASSEXW);
         wcex.style = CS_HREDRAW | CS_VREDRAW;
-        wcex.lpfnWndProc = WndProc;
+        wcex.lpfnWndProc = WndProcFriedmann;
         wcex.hInstance = hInstance;
         wcex.hIcon = LoadIconW(hInstance, L"IDI_ICON");
         wcex.hCursor = LoadCursorW(nullptr, IDC_ARROW);
         wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-        wcex.lpszClassName = L"Direct3DGameWindowClass";
+        wcex.lpszClassName = L"FriedmannWindowClass";
         wcex.hIconSm = LoadIconW(wcex.hInstance, L"IDI_ICON");
         if (!RegisterClassExW(&wcex))
             return 1;
 
         // Create window
         int w = 200, h = 50;
+        RECT rc = { 0, 0, static_cast<LONG>(w), static_cast<LONG>(h) };
 
+        AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE); //чтобы это были соотношения рабочей области!
 
+        HWND hwnd;
+
+        hwnd = CreateWindowExW(0, L"Direct3DGameWindowClass", L"Friedmann", WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance,
+            nullptr);
+
+        if (!hwnd)
+            return 1;
+
+        ShowWindow(hwnd, SW_SHOWNORMAL);
+        GetClientRect(hwnd, &rc);
 
     }
 
