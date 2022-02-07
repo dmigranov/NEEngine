7fee4ac586a2080c3dd31a3c671f34d8c0e68c26
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-07 15:19:08 +0700

    added color support to TextDrawer

--- NEEngine/src/TextDrawer.cpp
+++ NEEngine/src/TextDrawer.cpp
@@ -20,24 +20,24 @@ void TextDrawer::DrawTextUpRightAlign(const char* output, float x, float y, Dire
 	Vector2 origin = m_font->MeasureString(output)/* / 2.f*/;
 	origin.y = 0;
 	m_font->DrawString(m_spriteBatch.get(), output,
-		pos, Colors::Black, 0.f, origin);
+		pos, color, 0.f, origin);
 
 	m_spriteBatch->End();
 }
 
-void TextDrawer::DrawTextUpLeftAlign(const char* output, float x, float y)
+void TextDrawer::DrawTextUpLeftAlign(const char* output, float x, float y, DirectX::XMVECTOR color)
 {
 	m_spriteBatch->Begin();
 
 	Vector2 pos = Vector2(x, y);
 	Vector2 origin(0.f, 0.f); // = m_font->MeasureString(output);
 	m_font->DrawString(m_spriteBatch.get(), output,
-		pos, Colors::Black, 0.f, origin);
+		pos, color, 0.f, origin);
 
 	m_spriteBatch->End();
 }
 
-void TextDrawer::DrawTextDownLeftAlign(const char* output, float x, float y)
+void TextDrawer::DrawTextDownLeftAlign(const char* output, float x, float y, DirectX::XMVECTOR color)
 {
 	m_spriteBatch->Begin();
 
@@ -45,19 +45,19 @@ void TextDrawer::DrawTextDownLeftAlign(const char* output, float x, float y)
 	Vector2 origin = m_font->MeasureString(output);
 	origin.x = 0;
 	m_font->DrawString(m_spriteBatch.get(), output,
-		pos, Colors::Black, 0.f, origin);
+		pos, color, 0.f, origin);
 
 	m_spriteBatch->End();
 }
 
-void TextDrawer::DrawTextDownRightAlign(const char* output, float x, float y)
+void TextDrawer::DrawTextDownRightAlign(const char* output, float x, float y, DirectX::XMVECTOR color)
 {
 	m_spriteBatch->Begin();
 
 	Vector2 pos = Vector2(x, y);
 	Vector2 origin = m_font->MeasureString(output);
 	m_font->DrawString(m_spriteBatch.get(), output,
-		pos, Colors::Black, 0.f, origin);
+		pos, color, 0.f, origin);
 
 	m_spriteBatch->End();
 }
