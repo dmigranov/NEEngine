0be86a2868e21604056e6a818060dcebb575ee4a
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-08 18:16:43 +0700

    ладно, пусть некрасиво, но както работает. на сегодня хватит

--- NEEngine/inc/SphericalDopplerEffect.h
+++ NEEngine/inc/SphericalDopplerEffect.h
@@ -7,7 +7,7 @@ public:
     struct PerApplicationPSConstantBufferDoppler 
     {
         DirectX::XMVECTORF32 fogColor = DirectX::Colors::CadetBlue;
-        double velocity_coeff = 0.;
+        double velocity_coeff = 50000000.;
     };
 
     struct PerApplicationVSConstantBufferDoppler
