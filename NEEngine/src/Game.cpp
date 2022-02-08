a6d5e1019594373e4c6bb8496f64f71b025b82dc
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-08 13:22:27 +0700

    нет, дело не в этом..

--- NEEngine/src/Game.cpp
+++ NEEngine/src/Game.cpp
@@ -222,7 +222,7 @@ int Game::Initialize(HWND window, int width, int height)
     ZeroMemory(&samplerDesc, sizeof(D3D11_SAMPLER_DESC));
     samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
     samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
-    samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP; //D3D11_TEXTURE_ADDRESS_WRAP;
+    samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
     samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
     samplerDesc.MipLODBias = 0.0f;
     samplerDesc.MaxAnisotropy = 1;
