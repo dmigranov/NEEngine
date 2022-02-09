a12463799c5445740a802a1c80859e8d155b7e38
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-09 11:37:46 +0700

    added diag session file

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -122,7 +122,8 @@ int main(int argc, char* argv[])
     auto transformComponent = new SphericalTransformComponent(0, 0, 0.5);
     auto entity = new Entity();
     auto updaterComponent = new UpdaterComponent([transformComponent](double delta, Entity* pEntity) {
-        std::cout << "here" << std::endl;
+        
+        //std::cout << "here" << std::endl;
         });
     entity->AddComponent<SphericalTransformComponent>(transformComponent);
     entity->AddComponent<MeshComponent>(earth_mc);
