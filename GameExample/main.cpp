15824cb9d9f41ca88323afe256307af31808dc20
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-09 10:44:35 +0700

    включен vsync, но все равно мигания

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
     double radius = 1.;
 
     Game& game = Game::GetInstance();
-    game.InitializeEngine(L"Test game", true, false, false);
+    game.InitializeEngine(L"Test game", true, false, true);
     game.SetBackgroundColor(DirectX::Colors::Black);   //todo: перенести
     Scene* scene = game.GetScene();
     auto resourceManager = game.GetResourceManager();
