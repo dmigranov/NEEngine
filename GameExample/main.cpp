138043c1386f6a13ec89bac55f82ce03cb8d5c5f
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-09 14:11:40 +0700

    some

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -168,7 +168,7 @@ int main(int argc, char* argv[])
             auto old_radius = renderSystem->GetRadius();
             renderSystem->SetRadius(radius);
             auto oldToNewRadius = old_radius / radius;
-
+            oldToNewRadius = (1 - cos(mu - 0.03))/ (1 - cos(mu));;
             std::cout << oldToNewRadius << std::endl;
 
             int count = 0;
