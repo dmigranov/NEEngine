7ce93b325ca48e6f8767d45b9ebc79cccefc58f5
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-11 15:20:56 +0700

    getter and setter implemented

--- NEEngine/src/SphericalRenderingComponent.cpp
+++ NEEngine/src/SphericalRenderingComponent.cpp
@@ -1,2 +1,12 @@
 #include "pch.h"
 #include "SphericalRenderingComponent.h"
+
+SphericalVisibility SphericalRenderingComponent::GetSphericalVisibility()
+{
+    return m_sphericalVisibility;
+}
+
+void SphericalRenderingComponent::SetSphericalVisibility(SphericalVisibility sphVisibility)
+{
+    m_sphericalVisibility = sphVisibility;
+}
