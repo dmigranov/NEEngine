a41a8d97749713e57930d8f6c46c55d931b33ad0
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-11 15:17:08 +0700

    enum class SphericalVisibility

--- NEEngine/inc/SphericalRenderingComponent.h
+++ NEEngine/inc/SphericalRenderingComponent.h
@@ -1,5 +1,13 @@
 #pragma once
 #include "Component.h"
+
+enum class SphericalVisibility {
+	VISIBLE_NONE,
+	VISIBLE_FRONT,
+	VISIBLE_BACK,           //not really necessary but why not
+	VISIBLE_ALL
+};
+
 class SphericalRenderingComponent :
     public Component
 {
