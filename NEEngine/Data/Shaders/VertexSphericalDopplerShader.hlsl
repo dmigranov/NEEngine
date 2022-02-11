8d8b497801a1ec23302ab9d5366c890b339c9096
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-11 12:27:52 +0700

    поясняющий комментарий в шейдере

--- NEEngine/Data/Shaders/VertexSphericalDopplerShader.hlsl
+++ NEEngine/Data/Shaders/VertexSphericalDopplerShader.hlsl
@@ -99,10 +99,13 @@ VertexShaderOutput main(VertexShaderInput IN
 	OUT.tex = IN.tex;
 
 	double distanceCenter; //расстояние от наблюдателя до центра объекта, чтобы для всех его точек было одинаково (чтобы было разное - взять distance)
-	//я не прибавляю к нему pi, так как при вычислении разницы это pi устранится, 
-	//и формула ниже примет вид, как внизу
-
 	distanceCenter = SphericalDistance(float4(0, 0, 0, radius), mul(viewWorld, float4(0, 0, 0, radius)), radius);
+	//if (instanceID == 1)
+	//	distanceCenter += 3.14159265 * radius;	//я не прибавляю к расстоянию pi, так как
+	//при вычислении разницы это pi устранится, и формула ниже примет вид, как внизу
+	//lnew - lold = lnew' + pi - lold' - pi = lnew' - lold' = lnew' (1 - rold/rnew)
+	//lnew', lold' - без прибавления pi.
+
 	double distDiff = distanceCenter * (1. - radius_old / radius);
 	OUT.velocity = distDiff / deltaTime;
 
