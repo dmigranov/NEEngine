9849233becc1216b917557012bf0110227cffc3c
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-14 18:05:40 +0700

    deleted old code

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -278,40 +278,7 @@ int main(int argc, char* argv[])
                 {
                     Entity* sphere = entities[i];
                     auto pTransform = sphere->GetComponent<SphericalTransformComponent>();
-                    /*
-                    auto pos_world = pTransform->GetSphericalPosition();
-                    auto pos = Vector4::Transform(pos_world, view); //pos_view
-                    auto pos_w = pos.w;
-                    auto posProj_4D = Vector4(pos.x/pos_w, pos.y/pos_w, pos.z/pos_w, 1.f);
-                    auto posProj = Vector3(pos.x / pos_w, pos.y / pos_w, pos.z / pos_w);
 
-                    Vector3 centerToBeginning = posProj - rayStart; // rayStart = 0, so centerToBeginning = posProj
-                    double lenToCenterOfSphere_sq = pow(centerToBeginning.x, 2) + pow(centerToBeginning.y, 2) + pow(centerToBeginning.z, 2);
-                   
-                    // inside this sphere
-                    if (lenToCenterOfSphere_sq < r_projected_sq)
-                    {
-                        std::cout << "inside" << std::endl;
-                        continue;   //camera is inside of this sphere so no need
-                    }
-
-                    // ray starts outside this sphere
-                    float lenClosestPoint = centerToBeginning.Dot(direction);  //расстояние от начала луча до ближайшей к центру сферы точки луча
-                    //std::cout << lenClosestPoint << std::endl;
-                    if (lenClosestPoint < 0)
-                    {
-                        continue; //ray doesn't intersect
-                    }
-
-                    double thc_sq = r_projected_sq - lenToCenterOfSphere_sq + lenClosestPoint * lenClosestPoint;
-                    if (thc_sq < 0)
-                    {
-                        continue; //ray doesn't intersect
-                    }
-
-                    double thc = sqrt(thc_sq);
-                    double t = lenClosestPoint - thc; //t is the distance to the intersection point
-                    */
                     double t = RayTraceSpherePos(pTransform, rayStart, direction, view, r_projected_sq);
                     if (t < 0)
                     {
