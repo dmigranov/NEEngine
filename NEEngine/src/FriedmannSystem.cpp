260639d48254831b930980d7d91df3fb70ca36f8
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-20 13:46:04 +0700

    some FriedmannSystem() work

--- NEEngine/src/FriedmannSystem.cpp
+++ NEEngine/src/FriedmannSystem.cpp
@@ -1,8 +1,25 @@
 #include "pch.h"
 #include "FriedmannSystem.h"
 
+#include "Entity.h"
+#include "SphericalTransformComponent.h"
+#include "SphericalRenderingComponent.h"
+
+
 FriedmannSystem::FriedmannSystem() : ActionSystem([](Entity* pEntity, double deltaTime) {
-	
+    auto pTransform = pEntity->GetComponent<SphericalTransformComponent>();
+    auto pos = pTransform->GetSphericalPosition();
+    auto renderingComponent = sphere->GetComponent<SphericalRenderingComponent>();
+    auto visibility = renderingComponent->GetSphericalVisibility();
+
+    auto dist = SphericalDistance(pos / radius, cameraPos / radius, 1.);
+
+    if (mu < dist)
+        renderingComponent->SetSphericalVisibility(SphericalVisibility::VISIBLE_NONE);
+    else if (mu >= dist && mu <= (2 * XM_PI - dist))
+        renderingComponent->SetSphericalVisibility(SphericalVisibility::VISIBLE_FRONT);
+    else //mu > (2 * XM_PI - dist)
+        renderingComponent->SetSphericalVisibility(SphericalVisibility::VISIBLE_ALL);
 	
 	})
 {	}
