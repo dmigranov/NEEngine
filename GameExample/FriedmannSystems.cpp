6b4437932d895d781a111c8a71c6567c78de9737
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-21 11:08:22 +0700

    some

---
+++ GameExample/FriedmannSystems.cpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "main.h"
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
+void CreateFriedmannSystems(System** ControlSystem, System** RadiusSystem, System** VisibilitySystem)
+{
+
+}
