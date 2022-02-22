89e357c1d113f3c69346c7fec8540e5cccbc0d58
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-22 14:59:28 +0700

    some

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -215,29 +215,14 @@ int main(int argc, char* argv[])
                 Entity* sphere = entities[i];
                 auto pTransform = sphere->GetComponent<SphericalTransformComponent>();
                 const auto& world = pTransform->GetWorld();
-                auto sphericalPosition = pTransform->GetSphericalPosition();
 
-
-                Vector4 testVector1(0, r_sphere, 0, w_sphere);
-                Vector4 viewVector1 = Vector4::Transform(Vector4::Transform(testVector1, world), view);
-                Vector4 viewVector2 = Vector4::Transform(sphericalPosition, view);
-                Vector4 resVector1 = Vector4::Transform(viewVector1, proj);
-                Vector4 resVector2 = Vector4::Transform(viewVector2, proj);
-                if (resVector1.w == 0 || resVector2.w == 0)
-                    continue;
-                    
-                resVector1 /= resVector1.w;
-                resVector2 /= resVector2.w;
 
                 if (ms.leftButton && !pInput->IsRelative())
                 {
                     //std::cout << resVector1.x << " " << resVector1.y << " " << resVector1.z << std::endl;
                     //std::cout << resVector2.x << " " << resVector2.y << " " << resVector2.z << std::endl;
                     //double distance_sq = pow(resVector1.x - resVector2.x, 2) + pow(resVector1.y - resVector2.y, 2) ;
-                    //std::cout << distance_sq << std::endl;
-                    //std::cout << r_projected_sq << std::endl;
                 }
-                double distance_sq = pow(resVector1.x - resVector2.x, 2) + pow(resVector1.y - resVector2.y, 2) + pow(resVector1.z - resVector2.z, 2);
 
                 //double t = RayTraceSpherePos(sphericalPosition, rayStart, direction, view, proj, distance_sq);
                 
