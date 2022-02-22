12d3e9b3fda2336086cd4140d7db0736027c65db
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-22 15:41:58 +0700

    some

--- GameExample/main_raytracing.cpp
+++ GameExample/main_raytracing.cpp
@@ -110,12 +110,10 @@ double RayTraceSphereMouse(double mouseX, double mouseY, SphericalTransformCompo
     projectedRadiusVector2 /= projectedRadiusVector2.w;
     //std::cout << projectedRadiusVector1.x << " " << projectedRadiusVector1.y << " " << projectedRadiusVector1.z << std::endl;
     //std::cout << projectedRadiusVector2.x << " " << projectedRadiusVector2.y << " " << projectedRadiusVector2.z << std::endl;
-    std::cout << std::endl;
+
     auto distSq = pow(projectedRadiusVector1.x - projectedRadiusVector2.x, 2) + pow(projectedRadiusVector1.y - projectedRadiusVector2.y, 2) + pow(projectedRadiusVector1.z - projectedRadiusVector2.z, 2);
     distSq /= 4;
-    std::cout << distSq << std::endl;
 
-    //-x +x - 2r! !!!!
     if (pos.z < 0)  //!!
         return -1;
 
