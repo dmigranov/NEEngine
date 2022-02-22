862aa37963d39ee2c6137eff55767ebb95a704ad
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-22 12:03:28 +0700

    some

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -226,13 +226,18 @@ int main(int argc, char* argv[])
 
                     if (ms.leftButton && !pInput->IsRelative())
                     {
-                        //std::cout << resVector.x << " " << resVector.y << " " << resVector.z << " " << resVector.w << std::endl;
+                        //std::cout << resVector2.x << " " << resVector2.y << " " << resVector2.z << " " << resVector2.w << std::endl;
+
                         resVector1 /= resVector1.w;
                         resVector2 /= resVector2.w;
 
-                        std::cout << r_projected << std::endl;
-                        double distance_sq = pow(resVector1.x - resVector2.x, 2) + pow(resVector1.y - resVector2.y, 2) ;
-                        std::cout << sqrt(distance_sq) << std::endl;
+                        std::cout << resVector1.x << " " << resVector1.y << " " << resVector1.z << std::endl;
+                        std::cout << resVector2.x << " " << resVector2.y << " " << resVector2.z << std::endl;
+
+
+                        //std::cout << r_projected << std::endl;
+                        //double distance_sq = pow(resVector1.x - resVector2.x, 2) + pow(resVector1.y - resVector2.y, 2) ;
+                        //std::cout << sqrt(distance_sq) << std::endl;
                         //std::cout << sqrt(resVector.x * resVector.x + resVector.y * resVector.y) << std::endl;
 
                     }
