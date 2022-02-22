4b0d840e7b40fb1d581068008da04e669d7e56d2
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-22 11:59:12 +0700

    sphericalPosition

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -218,9 +218,9 @@ int main(int argc, char* argv[])
                 {
                     //r_sphere w_sphere 
                     Vector4 testVector1(0, r_sphere, 0, w_sphere);
-                    Vector4 testVector2(0, 0, 0, radius);
+                    //Vector4 testVector2(0, 0, 0, radius);
                     Vector4 viewVector1 = Vector4::Transform(Vector4::Transform(testVector1, world), view);
-                    Vector4 viewVector2 = Vector4::Transform(Vector4::Transform(testVector2, world), view);
+                    Vector4 viewVector2 = Vector4::Transform(sphericalPosition, view);
                     Vector4 resVector1 = Vector4::Transform(viewVector1, proj);
                     Vector4 resVector2 = Vector4::Transform(viewVector2, proj);
 
