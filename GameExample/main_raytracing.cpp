24e9a1d5d9b23ed233672a66c54d1ff668341c10
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-22 15:47:57 +0700

    deleted some

--- GameExample/main_raytracing.cpp
+++ GameExample/main_raytracing.cpp
@@ -102,7 +102,6 @@ double RayTraceSphereMouse(double mouseX, double mouseY, SphericalTransformCompo
     Matrix matrixPosRadius(1, 0, 0, 0, 0, w_sphere, 0, r_sphere, 0, 0, 1, 0, 0, -r_sphere, 0, w_sphere);
     Matrix matrixNegRadius(1, 0, 0, 0, 0, w_sphere, 0, -r_sphere, 0, 0, 1, 0, 0, r_sphere, 0, w_sphere);
 
-    //Vector4 radiusVector1(0, r_sphere, 0, w_sphere);
     Vector4 radiusVector1(r_sphere, 0, 0, w_sphere);
     Vector4 radiusVector2(-r_sphere, 0, 0, w_sphere);
     Vector4 viewRadiusVector1 = Vector4::Transform(Vector4::Transform(radiusVector1, world), view);
