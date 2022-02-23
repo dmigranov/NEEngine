164b473e4a1d5aae86de30ead183fe64495b02c5
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-23 11:47:16 +0700

    getting visibility

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -205,6 +205,7 @@ int main(int argc, char* argv[])
                 Entity* sphere = entities[i];
                 auto pTransform = sphere->GetComponent<SphericalTransformComponent>();
                 auto pRendering = sphere->GetComponent<SphericalRenderingComponent>();
+                auto visibility = pRendering->GetSphericalVisibility();
                 const auto& world = pTransform->GetWorld();
 
                 
