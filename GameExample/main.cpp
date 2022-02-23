a2a879499b5b198de1394132778eff499940c63a
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-23 13:33:05 +0700

    deleted old

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -69,7 +69,6 @@ int main(int argc, char* argv[])
     scene->AddEntity(cameraEntity);
 
     auto effect = new SphericalDopplerEffect(greenTexture, greenWithBlueTexture, 0.1, DirectX::Colors::Black);
-    //auto effectEarth = new SphericalDopplerEffect(greenTexture, 0.1, DirectX::Colors::Black);
     //auto effect = new SphericalExpFogEffect(earthTexture, 0.15, DirectX::Colors::Black);
 
     auto pointEffect = new SphericalExpFogPointEffect(0.1, DirectX::Colors::Black);
