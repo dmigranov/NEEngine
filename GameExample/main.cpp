f8c48b149a5c085ff01b3ba33dc84c5df07c586f
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-23 12:15:48 +0700

    допусловие

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -203,7 +203,7 @@ int main(int argc, char* argv[])
                 auto pTransform = sphere->GetComponent<SphericalTransformComponent>();
                 auto pRendering = sphere->GetComponent<SphericalRenderingComponent>();
                 auto visibility = pRendering->GetSphericalVisibility();
-                if (visibility == SphericalVisibility::VISIBLE_NONE)
+                if (visibility == SphericalVisibility::VISIBLE_NONE || !pEntity->IsVisible())
                     continue;
 
                 const auto& world = pTransform->GetWorld();
