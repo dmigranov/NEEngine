5905891d06cf8ce982fb3c62bfeb7354edc0726b
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-25 15:50:08 +0700

    отмена выделения при невидимости

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -266,7 +266,7 @@ int main(int argc, char* argv[])
                     if (selectedObjects[i])
                     {
                         entities[i]->SetVisible(false);
-
+                        selectedObjects[i] = false;
                     }
             }
 
