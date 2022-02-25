3f9beb7acee75cdc3d6cbdcffbddb06895a1c61d
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-25 16:48:46 +0700

    subscribed to componentes

--- NEEngine/src/SelectionSystem.cpp
+++ NEEngine/src/SelectionSystem.cpp
@@ -1,1 +1,14 @@
 #include "SelectionSystem.h"
+
+#include "SphericalTransformComponent.h"
+#include "SphericalRenderingComponent.h"
+
+SelectionSystem::SelectionSystem()
+{
+	SubscribeToComponentType<SphericalTransformComponent>();
+	SubscribeToComponentType<SphericalRenderingComponent>();
+}
+
+void SelectionSystem::Execute(double deltaTime)
+{
+}
