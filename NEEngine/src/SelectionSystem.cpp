0e7a66f807b485ab3d16c89366abf965ef0a19de
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-25 17:31:01 +0700

    some

--- NEEngine/src/SelectionSystem.cpp
+++ NEEngine/src/SelectionSystem.cpp
@@ -8,7 +8,7 @@
 #include "Scene.h"
 #include "Entity.h"
 
-
+using namespace DirectX;
 
 SelectionSystem::SelectionSystem(InputComponent* pInputComponent, double initialObjectRadius) : m_game(Game::GetInstance())
 {
@@ -27,6 +27,15 @@ void SelectionSystem::Execute(double deltaTime)
 	double radius = SphericalEffect::GetRadius();
 	auto cameraPos = m_pCameraTransform->GetSphericalPosition();
 
-	auto w_sphere = radius - 2 * radius * pow(sin(objectRadius / radius / 2), 2);
+	auto w_sphere = radius - 2 * radius * pow(sin(m_initialObjectRadius / radius / 2), 2);
 	auto r_sphere = sqrt(radius * radius - w_sphere * w_sphere);
+
+	float minDist = 100000.;
+	int minIndex = -1;
+
+	const auto& view = m_pCameraTransform->GetView();
+	const Matrix& proj = cameraComponent->GetProj();
+
+	int width, height;
+	game.GetWindowSize(width, height);
 }
