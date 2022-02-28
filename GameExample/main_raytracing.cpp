5b74876c5ecf4f6ca29b0385b5db06b9138697ac
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-28 11:45:34 +0700

    GetSphericalFromCartesian func

--- GameExample/main_raytracing.cpp
+++ GameExample/main_raytracing.cpp
@@ -5,6 +5,35 @@
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
 
+XMFLOAT3 GetSphericalFromCartesian(float x4, float x3, float x2, float x1)
+{
+    float x42 = x4 * x4;
+    float x22 = x2 * x2;
+    float x32 = x3 * x3;
+
+    float a1 = acosf(x1);
+    if (x2 == 0 && x3 == 0 && x4 == 0)
+        if (x1 > 0)
+            return Vector3(a1, 0, 0);
+        else
+            return Vector3(a1, XM_PI, XM_PI);
+
+    float a2 = acosf(x2 / sqrtf(x22 + x32 + x42));
+    if (x3 == 0 && x4 == 0)
+        if (x2 > 0)
+            return Vector3(a1, a2, 0);
+        else
+            return Vector3(a1, a2, XM_PI);
+
+    float a3;
+    if (x4 >= 0)
+        a3 = acosf(x3 / sqrtf(x32 + x42));
+    else
+        a3 = XM_2PI - acosf(x3 / sqrtf(x32 + x42));
+
+    return XMFLOAT3(a1, a2, a3);
+}
+
 
 double RayTraceSpherePos(DirectX::SimpleMath::Vector4 sphericalPosition, DirectX::SimpleMath::Vector3 rayStart, 
     DirectX::SimpleMath::Vector3 direction, 
