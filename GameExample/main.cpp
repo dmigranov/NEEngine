d1bdecfdc852f16a34701373a67e3e672770dd7d
Denis Migranov <d.migranov@g.nsu.ru>
2022-02-28 15:27:43 +0700

    some debug

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -105,7 +105,7 @@ int main(int argc, char* argv[])
 
         randomPoints[i] = point;
         auto transformComponent = new SphericalTransformComponent();
-        transformComponent->MoveAbsolute(point.x, point.y, point.z, point.w);
+        //transformComponent->MoveAbsolute(point.x, point.y, point.z, point.w); //!!
         const auto& world = transformComponent->GetWorld();
         auto transformed = Vector4::Transform(Vector4(0, objectRadius, 0, sqrt(radius * radius - objectRadius * objectRadius)), world); //pos_view
         auto sphCoords = GetSphericalFromCartesian(transformed.x / radius, transformed.y / radius, transformed.z / radius, transformed.w / radius);
