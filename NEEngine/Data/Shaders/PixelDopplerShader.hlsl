752bce21c26bf13af5dfb662021d1699060339b2
Denis Migranov <d.migranov@g.nsu.ru>
2022-03-01 14:37:21 +0700

    aha!

--- NEEngine/Data/Shaders/PixelDopplerShader.hlsl
+++ NEEngine/Data/Shaders/PixelDopplerShader.hlsl
@@ -189,7 +189,7 @@ float4 main(PixelShaderInput IN) : SV_TARGET
 	float3 rgbNew;
 	if (!isRedshift && !isBlueshift)
 	{
-		float3 hsvNew = float3((float)hueNew, 1, 1);
+		float3 hsvNew = float3((float)hueNew, 1., 1.);
 		rgbNew = hsv2rgb(hsvNew);
 	}
 	else if (isRedshift)
