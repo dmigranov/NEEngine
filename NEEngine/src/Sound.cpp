436575b4a062fad7f1792043e9353f37c030b404
Denis Migranov <d.migranov@g.nsu.ru>
2022-03-04 18:21:13 +0700

    Sound constructor fixed

--- NEEngine/src/Sound.cpp
+++ NEEngine/src/Sound.cpp
@@ -9,6 +9,5 @@ Sound::Sound(const wchar_t* waveFileName)
 {
 	auto& game = Game::GetInstance();
 
-	m_soundEffect = std::make_unique<SoundEffect>(m_audEngine.get(),
-		L"explo1.wav");
+	m_soundEffect = std::make_unique<SoundEffect>(game.m_audEngine.get(), waveFileName);
 }
