bd268212c1a7131a2135c5a9caf44c3cb6ff2e88
Denis Migranov <d.migranov@g.nsu.ru>
2022-03-04 18:23:53 +0700

    m_soundEffect created

--- NEEngine/inc/Sound.h
+++ NEEngine/inc/Sound.h
@@ -5,5 +5,6 @@ public:
 	Sound(const wchar_t* waveFileName);
 private:
 	std::unique_ptr<DirectX::SoundEffect> m_soundEffect;
+	std::unique_ptr<DirectX::SoundEffectInstance> m_soundEffectInstance;
 };
 
--- NEEngine/src/Sound.cpp
+++ NEEngine/src/Sound.cpp
@@ -10,4 +10,5 @@ Sound::Sound(const wchar_t* waveFileName)
 	auto& game = Game::GetInstance();
 
 	m_soundEffect = std::make_unique<SoundEffect>(game.m_audEngine.get(), waveFileName);
+	m_soundEffectInstance = m_soundEffect->CreateInstance();
 }
