2852a879b46bd22966dde0b57dd5dd2dbf8f7d84
Denis Migranov <d.migranov@g.nsu.ru>
2022-03-04 20:15:23 +0700

    void SetVolume(float volume);  started

--- NEEngine/inc/Sound.h
+++ NEEngine/inc/Sound.h
@@ -3,8 +3,11 @@ class Sound
 {
 public:
 	Sound(const wchar_t* waveFileName);
-	void Play(bool loop = false);
 	~Sound();
+
+	void Play(bool loop = false);
+
+	void SetVolume(float volume);
 private:
 	std::unique_ptr<DirectX::SoundEffect> m_soundEffect;
 	std::unique_ptr<DirectX::SoundEffectInstance> m_soundEffectInstance;
