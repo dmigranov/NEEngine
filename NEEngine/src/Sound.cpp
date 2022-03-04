8f5f2dce4d786100025d2c53e6230019e8b1a666
Denis Migranov <d.migranov@g.nsu.ru>
2022-03-04 18:17:05 +0700

    some

--- NEEngine/src/Sound.cpp
+++ NEEngine/src/Sound.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 #include "Sound.h"
 
+using namespace DirectX;
+
 Sound::Sound(const wchar_t* waveFileName)
 {
+	m_soundEffect = std::make_unique<SoundEffect>(m_audEngine.get(),
+		L"explo1.wav");
 }
