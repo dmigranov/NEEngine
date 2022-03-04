c848a647bce52d8b83d10460f3fac80793e8128d
Denis Migranov <d.migranov@g.nsu.ru>
2022-03-04 18:16:20 +0700

    fixed constructor of sound

--- NEEngine/inc/Sound.h
+++ NEEngine/inc/Sound.h
@@ -1,5 +1,9 @@
 #pragma once
 class Sound
 {
+public:
+	Sound(const wchar_t* waveFileName);
+private:
+	std::unique_ptr<DirectX::SoundEffect> m_soundEffect;
 };
 
--- NEEngine/src/Sound.cpp
+++ NEEngine/src/Sound.cpp
@@ -1,2 +1,6 @@
 #include "pch.h"
 #include "Sound.h"
+
+Sound::Sound(const wchar_t* waveFileName)
+{
+}
