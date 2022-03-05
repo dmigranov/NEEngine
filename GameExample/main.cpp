46e38125dc90626727a7c4f5ade8d862f2ddb9e8
Denis Migranov <d.migranov@g.nsu.ru>
2022-03-05 11:34:12 +0700

    лучше, но надо работать

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -49,7 +49,7 @@ int main(int argc, char* argv[])
     Texture* greenWithBlueTexture = resourceManager->CreateTexture(L"green.dds");
 
     //Sound* ambientSound = new Sound(L"NightAmbienceSimple_02.wav");
-    Sound* ambientSound = new Sound(L"c-note.wav");
+    Sound* ambientSound = new Sound(L"c-note2.wav");
     scene->AddAndPlayLoopedSound(ambientSound);
 
     auto renderSystem = new SphericalRenderSystem();
