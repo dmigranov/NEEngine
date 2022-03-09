8a8e477d38650e41c6c3bb6d9624155643fd8596
Denis Migranov <d.migranov@g.nsu.ru>
2022-03-09 12:29:30 +0700

    fixed error

--- GameExample/FriedmannSystems.cpp
+++ GameExample/FriedmannSystems.cpp
@@ -160,7 +160,7 @@ FriedmannTimer* CreateFriedmannSystems(SphericalDopplerEffect* sphericalEffect,
                 float rNewAddition = (muEnd - (mu < muStart ? muStart : (mu > muEnd ? muEnd : mu))) / epsilon; 
                 float rNew = initialObjectRadius + (bigRadius - initialObjectRadius) * rNewAddition;
                 scaleCoeff = rNew / initialObjectRadius;
-                wScaleCoeff = sqrt((1*1 - rNew * rNew)/(1*1 - initialObjectRadius * initialObjectRadius);;
+                wScaleCoeff = sqrt((1*1 - rNew * rNew)/(1*1 - initialObjectRadius * initialObjectRadius));
                 auto scaleMatrix = Matrix(scaleCoeff, 0, 0, 0,
                     0, scaleCoeff, 0, 0,
                     0, 0, scaleCoeff, 0,
