099b8de2af7803b434d953a5cdaabe2096c1ea8c
Denis Migranov <d.migranov@g.nsu.ru>
2022-03-20 18:01:31 +0700

    все равно хаотично

--- NEEngine/Data/Shaders/VertexSphericalDopplerShader.hlsl
+++ NEEngine/Data/Shaders/VertexSphericalDopplerShader.hlsl
@@ -120,7 +120,7 @@ VertexShaderOutput main(VertexShaderInput IN
 		// тут прибавлять ничего не надо, так как далее sin в квадрате, а sin^2 x = sin^2 (2pi-x) 
 
 		//double radiusOldCenter = RadiusFunction(mu + 0.3 - chiCenter); //0.3 - чтобы все не пересекалось из-за нулевого радиуса
-		double radiusOldCenter = RadiusFunction(mu + 0.3 - chiCenter);
+		double radiusOldCenter = RadiusAbridgedFunction(mu - chiCenter);
 
 		float w_new = radiusOldCenter * (1 - 2 * pow(sin(distanceFromPointToCenter / (2 * radiusOldCenter)), 2));
 
