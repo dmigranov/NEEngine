9ac70205b25b7287b9826615752dd59fbdc2103a
Denis Migranov <d.migranov@g.nsu.ru>
2022-04-11 11:16:23 +0700

    some

--- NEEngine/inc/Sound.h
+++ NEEngine/inc/Sound.h
@@ -3,6 +3,7 @@ class Sound
 {
 public:
 	Sound(const wchar_t* waveFileName);
+	Sound(std::function<void(int16_t*, int, int)> generateFunction);
 	~Sound();
 
 	void Play(bool loop = false);
@@ -14,5 +15,6 @@ public:
 private:
 	std::unique_ptr<DirectX::SoundEffect> m_soundEffect;
 	std::unique_ptr<DirectX::SoundEffectInstance> m_soundEffectInstance;
-};
 
+	private std::function<void(int16_t*, int, int)> m_generateFunction;
+};
