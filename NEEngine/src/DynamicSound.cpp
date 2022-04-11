593549c5d9403e8287ab2c016a7cc0bc357c2d3e
Denis Migranov <d.migranov@g.nsu.ru>
2022-04-11 11:48:38 +0700

    some

--- NEEngine/src/DynamicSound.cpp
+++ NEEngine/src/DynamicSound.cpp
@@ -15,8 +15,9 @@ DynamicSound::DynamicSound(std::function<void(int16_t*, int, int)> generateFunct
 	// PCM (»мпульсно-кодова€ модул€ци€) 44100 Hz, 16-bit, 1 channel
 	m_audioBytes.resize(44100 * 2);
 	m_dynamicSoundEffectInstance = std::make_unique<DynamicSoundEffectInstance>(game.m_audEngine.get(),
-		[this](DynamicSoundEffectInstance*)
+		[this](DynamicSoundEffectInstance* effect)
 		{
+			int count = effect->GetPendingBufferCount();
 			// 'Buffer needed' event handler
 			m_audioBytes;
 		},
