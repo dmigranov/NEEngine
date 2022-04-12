92abba6d252398c80247e9efe69c3da75c152b1e
Denis Migranov <d.migranov@g.nsu.ru>
2022-04-12 17:36:29 +0700

    commented old stuff

--- GameExample/FriedmannSystems.cpp
+++ GameExample/FriedmannSystems.cpp
@@ -305,9 +305,7 @@ SoundSystem::SoundSystem(Sound* pSound, SelectionSystem* pSelectionSystem, Spher
 
             double angle = (2.0 * XM_PI * freq) * time;
             //double factor = 0.5 * (sin(angle) + 1.0); //from 0 to 1
-            double factor = 
-                (double)(sampleCount/10 - j) / sampleCount * 10 *
-                (j > sampleCount / 10) ? 0 : sin(angle); //from -1 to 1
+            //double factor = (double)(sampleCount/2 - j) / sampleCount * 2 * (j > sampleCount / 2) ? 0 : sin(angle); //from -1 to 1
 
 
             // PCM: 16 bit
