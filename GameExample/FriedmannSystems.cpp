691d1c62b04f8ed88e0b3bdbb1b9b6fe83453896
Denis Migranov <d.migranov@g.nsu.ru>
2022-04-12 16:17:48 +0700

    some

--- GameExample/FriedmannSystems.cpp
+++ GameExample/FriedmannSystems.cpp
@@ -297,10 +297,19 @@ SoundSystem::SoundSystem(Sound* pSound, SelectionSystem* pSelectionSystem, Spher
 
         int16_t* ptr = data;
         double time = 0.0;
-        for (int j = 0; j < m_pSound->GetSampleCount(); ++j, ++ptr)
+
+        size_t sampleCount = m_pSound->GetSampleCount();
+
+        for (int j = 0; j < sampleCount; ++j, ++ptr)
         {
+            if (j > sampleCount / 2)
+            {
+                *ptr = int16_t(0);
+                time += timeStep;
+                continue;
+            }
             double angle = (2.0 * XM_PI * freq) * time;
-            double factor = (m_pSound->GetSampleCount() - j)/ m_pSound->GetSampleCount() * 0.5 * (sin(angle) + 1.0); //from 0 to 1
+            double factor = 0.5 * (sin(angle) + 1.0); //from 0 to 1
 
             // PCM: 16 bit
             // -32 767 … 32 767 
