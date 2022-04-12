3358007123cda8b55454da52c63efe54accb7600
Denis Migranov <d.migranov@g.nsu.ru>
2022-04-12 20:14:51 +0700

    some

--- GameExample/FriedmannSystems.cpp
+++ GameExample/FriedmannSystems.cpp
@@ -297,11 +297,11 @@ SoundSystem::SoundSystem(Sound* pSound, SelectionSystem* pSelectionSystem, Spher
         // прибавлять и % на currentTickMax
         // звук - только когда currentTick = 0!
         static unsigned int currentTick = 0;
-        static unsigned int currentTickMax = 1;
+        static unsigned int currentTickMax = 2;
         if (m_hasChiChanged)
         {
             currentTick = 0;
-            currentTickMax = 10; //todo
+            currentTickMax = 2; //todo
         }
               
         size_t sampleCount = m_pSound->GetSampleCount(); 
