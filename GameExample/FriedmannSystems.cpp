9d2f9e631df24b9bef106f8f72f2a9e9a8efd496
Denis Migranov <d.migranov@g.nsu.ru>
2022-04-13 11:59:10 +0700

    frequency works

--- GameExample/FriedmannSystems.cpp
+++ GameExample/FriedmannSystems.cpp
@@ -313,6 +313,9 @@ SoundSystem::SoundSystem(Sound* pSound, SelectionSystem* pSelectionSystem, Spher
         int repetitionStep = 0;
         int soundSampleCount = 50; //delta
         double knockFrequency = minKnockFrequency + m_currentChi / XM_2PI * (maxKnockFrequency - minKnockFrequency);
+        std::cout << knockFrequency << std::endl;
+
+        
         double mustBePlayedEverySeconds = 1. / knockFrequency;
         //unsigned int mustBePlayedEverySamples = 44100 * mustBePlayedEverySeconds; 
         unsigned int mustBePlayedEverySamples = sampleRate / length * mustBePlayedEverySeconds; //так больше операций, но не завязаны на 44100
