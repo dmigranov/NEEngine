c235444f6ce1763709ea9311acf9ecc92f4df63a
Denis Migranov <d.migranov@g.nsu.ru>
2022-04-13 13:54:46 +0700

    some deleted

--- GameExample/FriedmannSystems.cpp
+++ GameExample/FriedmannSystems.cpp
@@ -316,8 +316,7 @@ SoundSystem::SoundSystem(Sound* pSound, SelectionSystem* pSelectionSystem, Spher
         double knockFrequency = minKnockFrequency + (1 - m_currentChi / XM_2PI) * (maxKnockFrequency - minKnockFrequency);
         double mustBePlayedEverySeconds = 1. / knockFrequency;
 
-        //unsigned int mustBePlayedEverySamples = 44100 * mustBePlayedEverySeconds; 
-        unsigned int mustBePlayedEverySamples = (double)sampleRate / length * mustBePlayedEverySeconds; //так больше операций, но не завязаны на 44100
+        unsigned int mustBePlayedEverySamples = (double)sampleRate / length * mustBePlayedEverySeconds; //вместо 44100 * mustBePlayedEverySeconds; так больше операций, но не завязаны на 44100
 
         for (int j = 0; j < sampleRate; ++j, ++ptr)
         {
