5e2529ae90a23ffa9aacf0da382c9bdeba1998ea
Denis Migranov <d.migranov@g.nsu.ru>
2022-04-18 10:23:47 +0700

    m_audioBytesCurrent

--- NEEngine/inc/DynamicSound.h
+++ NEEngine/inc/DynamicSound.h
@@ -22,8 +22,9 @@ private:
     std::unique_ptr<DirectX::DynamicSoundEffectInstance> m_dynamicSoundEffectInstance;
     std::function<void(int16_t*, int, int)> m_generateFunction;
 
-    constexpr static size_t m_audioBytesCount = 3;
-
     size_t m_sampleCount;
+
+    constexpr static size_t m_audioBytesCount = 3;
+    size_t m_audioBytesCurrent = 0;
     std::vector<uint8_t> m_audioBytesArray[m_audioBytesCount];
 };
