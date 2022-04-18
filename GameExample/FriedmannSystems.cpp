7f00beab1e324df70060d241dc00957b7992e205
Denis Migranov <d.migranov@g.nsu.ru>
2022-04-18 15:13:58 +0700

    some preps

--- GameExample/FriedmannSystems.cpp
+++ GameExample/FriedmannSystems.cpp
@@ -313,10 +313,12 @@ SoundSystem::SoundSystem(Sound* pSound, SelectionSystem* pSelectionSystem, Spher
         
         int playSampleCount = 10; //delta
 
-        double distanceNormalized = 1 - m_currentChi * radius / XM_2PI / maxRadius;
         //double distanceNormalized = 1 - m_currentChi / XM_2PI;
-        double knockFrequency = minKnockFrequency + distanceNormalized * (maxKnockFrequency - minKnockFrequency);
-        //std::cout << knockFrequency << std::endl;
+        double distanceNormalized = 1 - m_currentChi * radius / XM_2PI / maxRadius;
+        double knockFrequency = minKnockFrequency + 
+            distanceNormalized * (maxKnockFrequency - minKnockFrequency);
+        std::cout << knockFrequency << std::endl;
+
         double mustBePlayedEverySeconds = 1. / knockFrequency;
 
         unsigned int mustBePlayedEverySamples = (double)sampleRate / length * mustBePlayedEverySeconds; //вместо 44100 * mustBePlayedEverySeconds; так больше операций, но не завязаны на 44100
