7bdfa6f2bb002355c022a417dd9f2beb6a418b12
Denis Migranov <d.migranov@g.nsu.ru>
2022-04-27 13:54:31 +0700

    some preps

--- NEEngine/inc/ResourceManager.h
+++ NEEngine/inc/ResourceManager.h
@@ -5,10 +5,10 @@ class ResourceManager
 {
 public:
     ResourceManager(ID3D11Device * device);
-    Texture* CreateTexture(const WCHAR* name);
+    //Texture* CreateTexture(const WCHAR* name);
     //Shader* CreateShaderFromHeader(const WCHAR* name);
     //Shader* CreateShaderFromCompiled(const WCHAR* name);
-
+    void AddTexture(Texture& pTexture);
     void Clean();
 private:
     ID3D11Device* g_d3dDevice;
