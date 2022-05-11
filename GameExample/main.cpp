9d60310c1268ddb0a5ac20b849816be2f6145113
Denis Migranov <d.migranov@g.nsu.ru>
2022-05-11 17:31:59 +0700

    some

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -1,21 +1,17 @@
 ﻿#pragma once
 
-#include "Geometries/SphericalEllipticGeometry.h"
+#include "Geometries/HyperbolicGeometry.h"
 #include "WalkComponent.h"
 #include "InputSystem.h"
-#include "TextPrintingSystem.h"
-#include "TextComponent.h"
-#include "RandomSphericalGenerator.h"
+#include "ActionSystem.h"
 
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
 
 int main(int argc, char* argv[])
 {
-    double radius = 1.;
-
     Game& game = Game::GetInstance();
-    game.InitializeEngine(1280, 720, L"Spherical & Elliptic Spaces", true, false, false);
+    game.InitializeEngine(1280, 720, L"Test game", true, false, false);
     game.SetBackgroundColor(DirectX::Colors::PowderBlue);   //todo: перенести
     Scene* scene = game.GetScene();
     auto resourceManager = game.GetResourceManager();
@@ -26,91 +22,84 @@ int main(int argc, char* argv[])
         componentTypeManager->RegisterComponentType<InputComponent>();
         componentTypeManager->RegisterComponentType<WalkComponent>();
 
-        componentTypeManager->RegisterComponentType<SphericalTransformComponent>();
-        componentTypeManager->RegisterComponentType<SphericalCameraComponent>();
+        componentTypeManager->RegisterComponentType<HyperbolicTransformComponent>();
+        componentTypeManager->RegisterComponentType<HyperbolicCameraComponent>();
 
-        componentTypeManager->RegisterComponentType<TextComponent>();
-
-        //MeshComponent - registered by default
+        //transfcomp и meshcom зарегистр по умолчанию... 
 
         componentTypeManager->SetTypeAdditionEnded();
     }
 
     Texture* earthTexture = new Texture(L"earth8k.dds");
 
-    auto renderSystem = new SphericalRenderSystem();
-    renderSystem->SetRadius(radius);
     scene->AddSystem(new InputSystem());
-    scene->AddSystem(renderSystem);
-    scene->AddSystem(new SphericalControlSystem(0.3, 1.3));
+    scene->AddSystem(new HyperbolicRenderSystem());
+    scene->AddSystem(new HyperbolicControlSystem(0.3, 1.3));
+    scene->AddSystem(new ActionSystem<InputComponent, HyperbolicTransformComponent, WalkComponent>(
+        [](Entity* pEntity, double deltaTime) {
+            auto pTransform = pEntity->GetComponent<HyperbolicTransformComponent>();
+            auto pInput = pEntity->GetComponent<InputComponent>();
+            auto kbs = pInput->GetKeyboardState();
+            auto ms = pInput->GetMouseState();
+            auto pWalk = pEntity->GetComponent<WalkComponent>();
+
+
+            Vector3 up(0, deltaTime * pWalk->m_movementGain, 0);
+            Vector3 right(deltaTime * pWalk->m_movementGain, 0, 0);
+            Vector3 fwd(0, 0, deltaTime * pWalk->m_movementGain);
+
+            if (kbs.R)
+                pTransform->Move(up);
+            if (kbs.F)
+                pTransform->Move(-up);
+
+        }));
+
 
     Entity* cameraEntity = new Entity("camera1");
-    auto cameraTransform = new SphericalTransformComponent();
-    auto cameraComponent = new SphericalCameraComponent();
-    cameraEntity->AddComponent<SphericalTransformComponent>(cameraTransform);
-    cameraEntity->AddComponent<SphericalCameraComponent>(cameraComponent);
+    auto cameraTransform = new HyperbolicTransformComponent();
+    auto cameraComponent = new HyperbolicCameraComponent();
+    cameraComponent->SetFovY(XM_PI/3);
+    cameraEntity->AddComponent<HyperbolicTransformComponent>(cameraTransform);
+    cameraEntity->AddComponent<HyperbolicCameraComponent>(cameraComponent);
     cameraEntity->AddComponent<InputComponent>(new InputComponent());
+    //cameraEntity->AddComponent<WalkComponent>(new WalkComponent(3, 4));
+
     scene->SetCamera(cameraEntity, cameraComponent);
     scene->AddEntity(cameraEntity);
 
 
-    auto effect = new SphericalExpFogEffect(earthTexture, 0.4, DirectX::Colors::PowderBlue);
+    auto effect = new HyperbolicExpFogEffect(earthTexture, 0.5, DirectX::Colors::PowderBlue);
+
 
     auto charWalkComponent = new WalkComponent(3, 4);
     auto charInputComponent = new InputComponent();
 
-    auto entity1 = new Entity(), entity2 = new Entity();
+    auto entity1 = new Entity(), entity2 = new Entity(), entity3 = new Entity(), entity4 = new Entity();
 
-    auto smc = SphericalMeshComponentFactory::CreateSphericalSphere(0.1, 20, 20);
+    auto smc = HyperbolicMeshComponentFactory::CreateHyperbolicSphere(1, 20, 20);
     smc->SetEffect(effect);
 
+    auto tc1 = new HyperbolicTransformComponent(0, 0, 0);
+    auto tc2 = new HyperbolicTransformComponent(0.7, 0, 0);
+    auto tc3 = new HyperbolicTransformComponent(1.4, 0, 0);
+    auto tc4 = new HyperbolicTransformComponent(2.1, 0, 0);
 
-    cameraComponent->SetFovY(XM_PI / 2); //эксперимент с видимостью
-    int sphereCount = 6;
-    for (int i = 0; i < sphereCount; i++)
-    {
-        auto transformComponent = new SphericalTransformComponent(0, 0, i * XM_PI / sphereCount);
-        auto entity = new Entity();
-        entity->AddComponent<SphericalTransformComponent>(transformComponent);
-        entity->AddComponent<MeshComponent>(smc);
-        scene->AddEntity(entity);
-    }
+    entity1->AddComponent<HyperbolicTransformComponent>(tc1);
+    entity1->AddComponent<MeshComponent>(smc);
+    scene->AddEntity(entity1);
 
+    entity2->AddComponent<HyperbolicTransformComponent>(tc2);
+    entity2->AddComponent<MeshComponent>(smc);
+    scene->AddEntity(entity2);
 
-    auto bigSphereMesh = SphericalMeshComponentFactory::CreateSphericalSphere(0.85, 30, 30);
-    bigSphereMesh->SetEffect(effect);
+    entity3->AddComponent<HyperbolicTransformComponent>(tc3);
+    entity3->AddComponent<MeshComponent>(smc);
+    scene->AddEntity(entity3);
 
-    {
-        auto transformComponent = new SphericalTransformComponent(-XM_PIDIV2, 0, 0);
-        auto entity = new Entity();
-        entity->AddComponent<SphericalTransformComponent>(transformComponent);
-        entity->AddComponent<MeshComponent>(bigSphereMesh);
-        scene->AddEntity(entity);
-    }
-
-    scene->AddSystem(new TextPrintingSystem());
-    auto textEntity = new Entity();
-    textEntity->AddComponent<TextComponent>(new TextComponent([effect](double) {
-        if (effect->GetMode())
-            return "Spherical Space";
-        else
-            return "Elliptic Space";
-        }, 10, 10, Alignment::UpLeft, DirectX::Colors::Black));
-    scene->AddEntity(textEntity);
-
-    scene->AddSystem(new ActionSystem<InputComponent>(
-        [effect, renderSystem](Entity* pEntity, double deltaTime) {
-            auto pInput = pEntity->GetComponent<InputComponent>();
-            auto kbs = pInput->GetKeyboardState();
-
-            if (kbs.D1)
-                effect->SetMode(true);
-            else if (kbs.D2)
-                effect->SetMode(false);
-
-
-
-        }));
+    entity4->AddComponent<HyperbolicTransformComponent>(tc4);
+    entity4->AddComponent<MeshComponent>(smc);
+    scene->AddEntity(entity4);
 
     return game.StartGame();
 }
