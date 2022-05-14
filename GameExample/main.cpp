b46665be6affef44a0ab7b5cf1b63da8d5c96dc6
Denis Migranov <d.migranov@g.nsu.ru>
2022-05-14 20:56:45 +0700

    some

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -91,23 +91,11 @@ int main(int argc, char* argv[])
     cameraComponent->SetFovY(XM_PI / 2); //эксперимент с видимостью
 
     RandomSphericalGenerator generator(radius);   
-    auto randomPoints = new Vector4[sphereCount];
     auto entities = new Entity * [sphereCount];
     for (int i = 0; i < sphereCount; i++)
     {
-    again:
         auto point = generator.GeneratePoint();
 
-        for (int j = 0; j < i; j++)
-        {
-            auto otherPoint = randomPoints[j];
-            auto distance = SphericalDistance(point, otherPoint, radius);
-
-            if (distance < 2 * objectRadius)
-                goto again;
-        }
-
-        randomPoints[i] = point;
         auto transformComponent = new SphericalTransformComponent();
         transformComponent->MoveAbsolute(point.x, point.y, point.z, point.w); //!!!
 
@@ -124,7 +112,6 @@ int main(int argc, char* argv[])
         entities[i] = entity;
     }
 
-    delete[] randomPoints;
 
 
     /*
