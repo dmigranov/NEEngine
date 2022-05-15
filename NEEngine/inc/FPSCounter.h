3c540891f81ab9a42e15932de3338ffd6620cb54
Denis Migranov <d.migranov@g.nsu.ru>
2022-05-15 11:47:15 +0700

    some

--- NEEngine/inc/FPSCounter.h
+++ NEEngine/inc/FPSCounter.h
@@ -6,8 +6,10 @@ public:
 	void Update();	//called each frame
 	unsigned int GetFPS();
 	double GetFrameTime();
+
+	unsigned int GetMeanFPS();
 private:
-	unsigned int m_fps, m_count;
+	unsigned int m_fps, m_count, m_meanFPS;
 	double m_frameTime;
 	DWORD m_startTime;
 
